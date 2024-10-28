637ad89f412aabd79bb828172637f5b8f100029f
Farezas <fareza087@gmail.com>
2024-10-28 10:20:49 +0700

    add: icon, reusable component

---
+++ components/halalCommon/HalalModal.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+  parentBtnLabel: String,
+  cardItem: Object,
+  cardWidth: Number,
+})
+</script>
+
+<template>
+  <div>
+    <VBtn>
+      {{ props.parentBtnLabel }}
+      <VDialog
+        activator="parent"
+        :max-width="props.cardWidth"
+      >
+        <VCard :title="props.title">
+          <VCardItem>
+            <slot name="content" />
+          </VCardItem>
+        </VCard>
+      </VDialog>
+    </VBtn>
+  </div>
+</template>
---
+++ components/halalCommon/KembaliButton.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable regex/invalid -->
+<template>
+  <VBtn
+    variant="text"
+    prepend-icon="mdi-chevron-left"
+    @click="$router.go(-1)"
+  >
+    Kembali
+  </VBtn>
+</template>
