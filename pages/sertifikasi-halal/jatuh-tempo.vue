378cd4c7afa27148fbbdf8c03cae44efd0a0343d
FarezasCode <fareza087@gmail.com>
2024-10-28 04:37:28 +0000

    add: jatuh tempo, invoice page
    (cherry picked from commit 22ff436c01cc5570727a15c26673e2793a44f854)
    
    Co-authored-by: Farezas <fareza087@gmail.com>

---
+++ pages/sertifikasi-halal/jatuh-tempo.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const items = [
+  {
+    'No': 1,
+    'No. Sertifikat': '1232131jnjadd',
+    'Tanggal Terbit': '2024-10-05',
+    'Jatuh Tempo': '2024-12-05',
+    'Nama PU': 'wdbadbaiwd',
+    'Jenis Produk': 'awdbuabdiawbdi',
+    'Hari': 'awidbiabdiw',
+  },
+]
+</script>
+
+<template>
+  <div>
+    <VRow>
+      <VCol><KembaliButton /></VCol>
+    </VRow>
+    <VRow>
+      <VCol><h1>Sertifikat Halal Jatuh Tempo</h1></VCol>
+    </VRow>
+    <VRow>
+      <VCard class="w-100">
+        <VCardTitle>Daftar Sertifikat Jatuh Tempo</VCardTitle>
+        <VCardItem>
+          <VRow><VCol><VTextField label="Searching" /></VCol></VRow>
+          <VRow>
+            <VCol>
+              <VDataTable :items="items" />
+            </VCol>
+          </VRow>
+        </VCardItem>
+      </VCard>
+    </VRow>
+  </div>
+</template>
---
+++ pages/sertifikasi-halal/tagihan-invoice.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const items = [
+  {
+    'No': 1,
+    'No. Pembayaran': '1232131jnjadd',
+    'No.Tagihan': '',
+    'Tanggal Tagihan': '2024-10-05',
+    'No. Ref': '',
+    'Nama PU': 'wdbadbaiwd',
+    'Jenis Transaksi': '',
+    'Jatuh Tempo': '2024-12-05',
+    'Jumlah Tagihan': '',
+    'Status': '',
+    'Catatan': '',
+  },
+]
+</script>
+
+<template>
+  <div>
+    <VRow>
+      <!-- <VCol><VBtn>Kembali</VBtn></VCol> -->
+      <VCol><KembaliButton /></VCol>
+    </VRow>
+    <VRow>
+      <VCol><h1>Tagihan/Invoice</h1></VCol>
+    </VRow>
+    <VRow>
+      <VCol>
+        <VCard class="w-100">
+          <VCardTitle>Download Cara Bayar</VCardTitle>
+          <VCardItem>
+            <VRow>
+              <VCol class="v-col-3">
+                <p>Dokumen Cara Bayar</p>
+              </VCol>
+              <VCol class="v-col-1">
+                <p>:</p>
+              </VCol>
+              <VCol class="align-self-start ">
+                <VBtn>Download</VBtn>
+              </VCol>
+            </VRow>
+          </VCardItem>
+        </VCard>
+      </VCol>
+    </VRow>
+    <br>
+    <VRow>
+      <VCol>
+        <VCard class="w-100">
+          <VCardTitle>Daftar Tagihan/Invoice</VCardTitle>
+          <VCardItem>
+            <VRow><VCol><VTextField label="Searching" /></VCol></VRow>
+            <VRow>
+              <VCol>
+                <VDataTable :items="items" />
+              </VCol>
+            </VRow>
+          </VCardItem>
+        </VCard>
+      </VCol>
+    </VRow>
+  </div>
+</template>
