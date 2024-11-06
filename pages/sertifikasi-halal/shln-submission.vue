a46e3d4e9b1e3fcfb018506927c483a076f93b90
amevide998 <hdsvidi@gmaill.com>
2024-11-06 21:31:16 +0700

    shln template

--- pages/sertifikasi-halal/shln-submission.vue
+++ pages/sertifikasi-halal/shln-submission.vue
@@ -1,16 +1,41 @@
 <script setup lang="ts">
 
-const items = [{
+import NewRequestDialog from "@/views/pages/shln/NewRequestDialog.vue"
+
+const items = ref([{
   'No' : '1',
   'Submission Number' : 'xxxx',
   'NIB/Business Id No' : 'xxxxx',
   'NPWP/Taxpayer id No.' : 'xxxxx',
   'Date' : 'xxxxx',
   'Action' : 'xxxx'
-},]
+},])
 
+const searchQuery = ref('')
 
+const filteredItems = computed(() => {
+  if (!searchQuery.value)
+    return items.value
 
+  return items.value.filter(item =>
+    Object.values(item).some(value =>
+      String(value).toLowerCase().includes(searchQuery.value.toLowerCase()),
+    ),
+  )
+})
+
+const requestDialogVisible = ref(false)
+
+const router = useRouter()
+const hanleSubmitRequest = answer => {
+  console.log('answer request : ', answer)
+  router.push("/sertifikasi-halal/shln-detail")
+}
+
+const openModalsRequest = () => {
+  console.log("TEST ")
+  requestDialogVisible.value = true
+}
 </script>
 
 <template>
@@ -32,7 +57,7 @@ const items = [{
           <VBtn
             color="primary"
             append-icon="ri-add-line"
-            @click="openModalsQuestionare"
+            @click="openModalsRequest"
           >
             Add Submission
           </VBtn>
@@ -40,10 +65,16 @@ const items = [{
       </VRow>
       <VRow>
         <VCol>
-          <VDataTable :items-per-page-options="5" :items="items"/>
-        </VCol>
+          <VDataTable :items="filteredItems"/>
+        </VCol>v
       </VRow>
     </VCard>
+    <NewRequestDialog
+      :dialog-visible="requestDialogVisible"
+      :submit="hanleSubmitRequest"
+      @update:dialog-visible="requestDialogVisible = $event"
+    />
+
   </div>
 </template>
 
