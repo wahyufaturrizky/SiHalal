a4bd685752cc8544885caab43443fc9d61dbc7ff
Farezas <fareza087@gmail.com>
2024-11-08 10:17:27 +0700

    feat: layouting sh domestic self declare
    1. edit layout
    2. edit pelaku usaha tab
    3. edit pengajuan tab

---
+++ components/domesticSelfDecl/edit/pelakuUsaha/PelakuUsahaMain.vue
@@ -0,0 +1,33 @@
+<template>
+  <VContainer>
+    <div style="max-height: 50svh; overflow-y: auto">
+      <VRow>
+        <VCol :cols="12">
+          <VCard height="20svh" title="Data Pelaku Usaha">
+            <VCardText>tes</VCardText>
+          </VCard>
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol :cols="12">
+          <VCard title="Penanggung Jawab"></VCard>
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol :cols="12">
+          <VCard title="Aspek Legal"></VCard>
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol :cols="12">
+          <VCard title="Penyelia Halal"></VCard>
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol :cols="12">
+          <VCard title="Dokumen Persyaratan"></VCard>
+        </VCol>
+      </VRow>
+    </div>
+  </VContainer>
+</template>
