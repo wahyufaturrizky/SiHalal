d8ce3b911fde477e301266a246aef9763de9e82c
Farezas <fareza087@gmail.com>
2024-11-19 09:20:08 +0700

    add: detail fasilitasi modal

---
+++ components/fasilitator/detail-facilitate-modal-send.vue
@@ -0,0 +1,30 @@
+<template>
+  <VDialog max-width="50svw">
+    <template #activator="{ props: openModal }">
+      <VBtn
+        density="compact"
+        style="margin: 0.5svw"
+        variant="flat"
+        append-icon="fa-paper-plane"
+        v-bind="openModal"
+        >Kirim</VBtn
+      >
+    </template>
+    <template #default="{ isActive }">
+      <VCard>
+        <VCardTitle><h3>Kirim Pengajuan</h3></VCardTitle>
+        <VCardItem><p>Yakin ingin mengirim pengajuan fasilitasi?</p></VCardItem>
+        <VCardActions>
+          <VBtn
+            variant="outlined"
+            density="compact"
+            @click="isActive.value = false"
+          >
+            Batal</VBtn
+          >
+          <VBtn variant="flat" density="compact">Kirim</VBtn>
+        </VCardActions>
+      </VCard>
+    </template>
+  </VDialog>
+</template>
