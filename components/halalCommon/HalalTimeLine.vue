a161c7c5772c036a24c84106b898f9d582a06a7c
geryn25 <35487884+geryn25@users.noreply.github.com>
2024-11-22 19:03:45 +0700

    update list shln, submission shln, detail shln

--- components/halalCommon/HalalTimeLine.vue
+++ components/halalCommon/HalalTimeLine.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import type { ShlnTracking } from "@/pages/sertifikasi-halal/shln-detail/[id].vue";
+
+const props = defineProps<{
+  event: ShlnTracking[];
+}>();
+console.log(props.event);
 </script>
 
 <template>
@@ -12,51 +18,25 @@
       class="v-timeline--variant-outlined"
     >
       <VTimelineItem
+        v-for="item in event"
         dot-color="rgb(var(--v-theme-surface))"
         size="x-small"
       >
         <template #icon>
-          <VIcon
-            icon="ri-circle-line"
-            color="primary"
-            size="16"
-          />
+          <VIcon icon="ri-circle-line" color="primary" size="16" />
         </template>
-        <div class="d-flex justify-space-between align-center gap-2 flex-wrap mb-2">
-            <span class="app-timeline-title">
-              Register
-            </span>
-          <span class="app-timeline-meta">2024/10/05</span>
+        <div
+          class="d-flex justify-space-between align-center gap-2 flex-wrap mb-2"
+        >
+          <span class="app-timeline-title">
+            {{ item.status }}
+          </span>
+          <span class="app-timeline-meta">{{ item.created_at }}</span>
         </div>
 
         <div class="app-timeline-text mt-1">
-          SAMSUL
+          {{ item.username }}
         </div>
-
-      </VTimelineItem>
-      <VTimelineItem
-        dot-color="rgb(var(--v-theme-surface))"
-        size="x-small"
-      >
-        <template #icon>
-          <VIcon
-            icon="ri-circle-line"
-            color="primary"
-            size="16"
-          />
-        </template>
-        <div class="d-flex justify-space-between align-center gap-2 flex-wrap mb-2">
-            <span class="app-timeline-title">
-              Draft
-            </span>
-          <span class="app-timeline-meta">2024/09/10</span>
-        </div>
-
-        <!-- 👉 Content -->
-        <div class="app-timeline-text mt-1">
-          SAMSUL
-        </div>
-
       </VTimelineItem>
     </VTimeline>
   </div>
