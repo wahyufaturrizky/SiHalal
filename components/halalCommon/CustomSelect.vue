1328746a6c8ca5c1e1404d54bbbf6eb1f0eb0fde
amevide998 <hdsvidi@gmaill.com>
2024-11-24 01:03:54 +0700

    create custom form input

---
+++ components/halalCommon/CustomSelect.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  modelValue: { type: [String, Number], default: '' },
+  label: { type: String, required: true },
+  id: { type: String, required: true },
+  placeholder: { type: String, default: 'Pilih' },
+  rules: { type: Array, default: () => [] },
+  items: { type: Array, default: () => [] },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const internalValue = ref(props.modelValue)
+
+watch(internalValue, newValue => {
+  emit('update:modelValue', newValue)
+})
+</script>
+
+<template>
+  <div>
+    <VLabel
+      :for="id"
+      class="font-weight-bold"
+    >
+      {{ label }}
+    </VLabel>
+    <VSelect
+      :id="id"
+      v-model="internalValue"
+      :placeholder="placeholder"
+      :rules="rules"
+      :items="items"
+      clearable
+    >
+      <template #message="{ message }">
+        <div>
+          <i class="ri-error-warning-fill" /> {{ message }}
+        </div>
+      </template>
+    </VSelect>
+  </div>
+</template>
