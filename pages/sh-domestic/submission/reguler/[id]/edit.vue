e0f4345865ed719eeef737f1eed8741e08a75110
amevide998 <hdsvidi@gmaill.com>
2024-11-24 01:02:43 +0700

    create page edit pengajuan halal

--- pages/sh-domestic/submission/reguler/[id]/edit.vue
+++ pages/sh-domestic/submission/reguler/[id]/edit.vue
@@ -1,14 +1,49 @@
 <script setup lang="ts">
-
+const tabs = ref(0);
+onMounted(() => {
+  tabs.value = 0;
+});
 </script>
-
 <template>
-  <div>
-    EDIT DATA PENGAJUAN
-  </div>
+  <KembaliButton />
+  <p class="text-h2 font-weight-bold mb-4">Ubah Pengajuan Reguler</p>
+  <VTabs v-model="tabs" align-tabs="start" class="mb-4">
+    <VTab value="1">Data Pengajuan</VTab>
+    <VTab value="2">Komitmen dan Tanggung Jawab </VTab>
+    <VTab value="3">Bahan</VTab>
+    <VTab value="4">Proses</VTab>
+    <VTab value="5">Produk</VTab>
+    <VTab value="6">Pemantauan dan Evaluasi</VTab>
+    <VTab value="7">Dokumen</VTab>
+  </VTabs>
+  <VTabsWindow v-model="tabs">
+    <br />
+    <VTabsWindowItem value="1">
+      <EditDataPengajuanReguler />
+    </VTabsWindowItem>
 
+    <VTabsWindowItem value="2">
+      <EditKomitmenDanTanggungJawabReguler />
+    </VTabsWindowItem>
+
+    <VTabsWindowItem value="3">
+      <EditBahanReguler />
+    </VTabsWindowItem>
+
+    <VTabsWindowItem value="4">
+      <EditProsesReguler />
+    </VTabsWindowItem>
+
+    <VTabsWindowItem value="5">
+      <EditProdukReguler />
+    </VTabsWindowItem>
+
+    <VTabsWindowItem value="6">
+      <EditPemantauanDanEvaluasiReguler />
+    </VTabsWindowItem>
+
+    <VTabsWindowItem value="7">
+      <EditDokumenReguler />
+    </VTabsWindowItem>
+  </VTabsWindow>
 </template>
-
-<style scoped lang="scss">
-
-</style>
