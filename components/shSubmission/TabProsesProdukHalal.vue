050eae8c1bfe7d3563bb8ae3eaff0e4419e98350
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2024-11-23 20:08:37 +0700

    add tab proses produk halal for sh submission detail (pendamping)

---
+++ components/shSubmission/TabProsesProdukHalal.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <VCard class="pb-5">
+    <VCardTitle class="mb-5">
+      <div class="font-weight-bold text-h4">Proses Produk Halal</div>
+    </VCardTitle>
+    <VCardText>
+      <VItemGroup>
+        <VLabel>Proses</VLabel>
+        <VTextField placeholder="Masukkan Proses" density="compact">
+          <template #append>
+            <VBtn variant="outlined">Tambah</VBtn>
+          </template>
+        </VTextField>
+      </VItemGroup>
+      <br />
+      <VItemGroup>
+        <VLabel>Proses Produksi</VLabel>
+        <VTextarea placeholder="Masukkan Proses Produksi" density="compact" />
+      </VItemGroup>
+    </VCardText>
+    <VCardActions class="pe-5">
+      <VSpacer />
+      <VBtn variant="flat" text="Simpan Hasil Verifikasi PPh" class="px-4" />
+    </VCardActions>
+  </VCard>
+</template>
+
+<style scoped></style>
