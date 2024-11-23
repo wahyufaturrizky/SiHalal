5bf3402c781b05e690f89210a854ad975faa66ba
arvin018 <arvin.aditama@gmail.com>
2024-11-23 21:15:13 +0700

    Update register.vue

--- pages/register.vue
+++ pages/register.vue
@@ -1,58 +1,94 @@
 <script setup lang="ts">
-import { themeConfig } from "@themeConfig";
-import { useDisplay } from "vuetify";
-import { VForm } from "vuetify/components/VForm";
+import { themeConfig } from '@themeConfig'
+import { useDisplay } from 'vuetify'
+import { VForm } from 'vuetify/components/VForm'
 
-import { requiredValidator } from "#imports";
-import { VNodeRenderer } from "@/@layouts/components/VNodeRenderer";
-import onlyAcceptNumber from "@/utils/onlyAcceptNumber";
-import bseImage from "@images/bse.png";
-import NoImage from "@images/no-image.png";
-import ossImage from "@images/oss.png";
-import authV2LoginIllustrationBorderedDark from "@images/pages/auth-v2-login-illustration-bordered-dark.png";
-import authV2LoginIllustrationBorderedLight from "@images/pages/auth-v2-login-illustration-bordered-light.png";
-import authV2LoginIllustrationDark from "@images/pages/auth-v2-login-illustration-dark.png";
-import authV2LoginMaskDark from "@images/pages/auth-v2-login-mask-dark.png";
-import authV2LoginMaskLight from "@images/pages/auth-v2-login-mask-light.png";
+import { requiredValidator } from '#imports'
+import { VNodeRenderer } from '@/@layouts/components/VNodeRenderer'
+import bseImage from '@images/bse.png'
+import NoImage from '@images/no-image.png'
+import ossImage from '@images/oss.png'
+import authV2LoginIllustrationBorderedDark from '@images/pages/auth-v2-login-illustration-bordered-dark.png'
+import authV2LoginIllustrationBorderedLight from '@images/pages/auth-v2-login-illustration-bordered-light.png'
+import authV2LoginIllustrationDark from '@images/pages/auth-v2-login-illustration-dark.png'
+import authV2LoginMaskDark from '@images/pages/auth-v2-login-mask-dark.png'
+import authV2LoginMaskLight from '@images/pages/auth-v2-login-mask-light.png'
 
-const { mdAndUp } = useDisplay();
+const { mdAndUp } = useDisplay()
 
 const authThemeImg = useGenerateImageVariant(
   NoImage,
   authV2LoginIllustrationDark,
   authV2LoginIllustrationBorderedLight,
   authV2LoginIllustrationBorderedDark,
-  true
-);
+  true,
+)
 
 const authThemeMask = useGenerateImageVariant(
   authV2LoginMaskLight,
-  authV2LoginMaskDark
-);
+  authV2LoginMaskDark,
+)
 
 definePageMeta({
-  layout: "blank",
+  layout: 'blank',
   unauthenticatedOnly: true,
-});
+})
 
-const isPasswordVisible = ref(false);
-const isPasswordVisibleConfrim = ref(false);
+const isPasswordVisible = ref(false)
+const isPasswordVisibleConfrim = ref(false)
 
-const route = useRoute();
+const route = useRoute()
 
-const ability = useAbility();
+const ability = useAbility()
 
-const errors = ref<Record<string, string | undefined>>({
+const errors = reactive({
   email: undefined,
   typeUser: undefined,
   name: undefined,
   noHandphone: undefined,
   password: undefined,
   passwordConfirm: undefined,
-});
+})
+
+const validateName = () => {
+  if (!form.value.name)
+    errors.name = 'Wajib diisi'
+  else
+    errors.name = ''
+}
+
+const validateEmail = () => {
+  if (!form.value.email)
+    errors.email = 'Wajib diisi'
+  else
+    errors.email = ''
+}
+
+// validateNomorhandphone
+const validateNomorHandphone = () => {
+  if (!form.value.noHandphone)
+    errors.noHandphone = 'Wajib diisi'
+  else
+    errors.noHandphone = ''
+}
+
+const validatePassword = () => {
+  if (!form.value.password)
+    errors.password = 'Wajib diisi'
+  else
+    errors.password = ''
+}
+
+// validateConfrimPassword
+const validateConfrimPassword = () => {
+  if (!form.value.passwordConfirm)
+    errors.passwordConfirm = 'Wajib diisi'
+  else
+    errors.passwordConfirm = ''
+}
 
 // const turnstile = ref();
-const refVForm = ref<VForm>();
+const refVForm = ref<VForm>()
 
 const form = ref({
   typeUser: null,
@@ -61,12 +97,12 @@ const form = ref({
   noHandphone: null,
   password: null,
   passwordConfirm: null,
-});
+})
 
-const router = useRouter();
+const router = useRouter()
 
 const onSubmit = async () => {
-  localStorage.setItem("formData", JSON.stringify(form.value));
+  localStorage.setItem('formData', JSON.stringify(form.value))
 
   refVForm.value?.validate().then(async ({ valid: isValid }) => {
     if (isValid) {
@@ -88,15 +124,15 @@ const onSubmit = async () => {
         phone_number: form.value.noHandphone,
         password: form.value.password,
         confirm_password: form.value.passwordConfirm,
-      };
+      }
 
-      const id = payload.role_id;
-      const email = payload.email;
+      const id = payload.role_id
+      const email = payload.email
 
       navigateTo({
-        path: "/verifikasi-user",
+        path: '/verifikasi-user',
         query: { id, email, payload: JSON.stringify(payload) },
-      });
+      })
 
       // try {
       //   const response = await $api("/auth/register", {
@@ -141,22 +177,22 @@ const onSubmit = async () => {
       //   console.error("Error saat membuat akun:", error);
       // }
     }
-  });
-};
+  })
+}
 
-const typeUserItem = ["Pelaku Usaha", "Buisness Actor", "Impoter"];
+const typeUserItem = ['Pelaku Usaha', 'Buisness Actor', 'Impoter']
 
 // check  disableSubmit
 const isDisabledSubmit = computed(() => {
   return !(
-    form.value.typeUser &&
-    form.value.name &&
-    form.value.email &&
-    form.value.noHandphone &&
-    form.value.password &&
-    form.value.passwordConfirm
-  );
-});
+    form.value.typeUser
+    && form.value.name
+    && form.value.email
+    && form.value.noHandphone
+    && form.value.password
+    && form.value.passwordConfirm
+  )
+})
 
 // validasi
 // const phoneValidator = (value: string) => {
@@ -169,67 +205,76 @@ const isDisabledSubmit = computed(() => {
 // };
 
 const emailValidator = (value: string) => {
-  if (value.includes("-"))
-    return "Format email tidak bisa menggunakan dash (-)";
+  if (value.includes('-'))
+    return 'Format email tidak bisa menggunakan dash (-)'
 
-  const isValid =
-    /^(?!.*[.]{2})[a-z0-9.]+@[a-z]+.(com|co.id|go.id|id|net)$/.test(value);
+  const isValid
+    = /^(?!.*[.]{2})[a-z0-9.]+@[a-z]+.(com|co.id|go.id|id|net)$/.test(value)
 
-  return isValid || " Format Email digunakan salah";
-};
+  return isValid || ' Format Email digunakan salah'
+}
 
-const requiredValidator = (value: string) => !!value || "Wajib diisi";
+const requiredValidator = (value: string) => !!value || 'Wajib diisi'
 
 const requiredSamePassword = (value: string) =>
-  value === form.value.password || "Kata sandi tidak sama!";
+  value === form.value.password || 'Kata sandi tidak sama!'
 
 const requiredMinLength = (value: string) =>
-  value.length >= 8 || "Pastikan kata sandi minimal 8 karakter!";
+  value.length >= 8 || 'Pastikan kata sandi minimal 8 karakter!'
 
 const requiredValidasinoHP = (value: string) =>
-  value !== "08123456789" ||
-  "Nomor handphone sudah terdaftar, gunakan nomor lain!";
+  value !== '08123456789'
+  || 'Nomor handphone sudah terdaftar, gunakan nomor lain!'
 
 const requiredValidasiEmail = (value: string) =>
-  value !== "hallal@gmail.com" ||
-  "Email sudah terdaftar,silahkan gunakan email lain!";
+  value !== 'hallal@gmail.com'
+  || 'Email sudah terdaftar,silahkan gunakan email lain!'
 
 // Gagal melakukan pembuatan akun, mohon periksa kembali kelengkapan data!
 // jika error pas di input maka tapil diatas
-const { sendSnackbar } = useSnackbar();
+const { sendSnackbar } = useSnackbar()
 
 onMounted(async () => {
   try {
-    const response: any = await $api("/auth/type-role", {
-      method: "GET",
+    const response: any = await $api('/auth/type-role', {
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     }).then((resp: any) => {
-      console.log("fetch type = ", resp);
-      fetchType.value = resp?.filter((val: any) => val.name !== ""); // Menyimpan data ke dalam ref
-    });
-  } catch (error) {
-    console.error("Gagal mengambil data:", error);
+      console.log('fetch type = ', resp)
+      fetchType.value = resp?.filter((val: any) => val.name !== '') // Menyimpan data ke dalam ref
+    })
   }
-});
+  catch (error) {
+    console.error('Gagal mengambil data:', error)
+  }
+})
 
 onMounted(() => {
-  const savedData = localStorage.getItem("formData");
-  if (savedData) form.value = JSON.parse(savedData);
-});
+  const savedData = localStorage.getItem('formData')
+  if (savedData)
+    form.value = JSON.parse(savedData)
+})
 
-const fetchType = ref([]);
+const fetchType = ref([])
 </script>
 
 <template>
-  <VRow no-gutters class="auth-wrapper">
+  <VRow
+    no-gutters
+    class="auth-wrapper"
+  >
     <VCol
       cols="12"
       md="6"
       class="auth-card-v2 d-flex align-center justify-center bg-white"
     >
-      <VCard flat :max-width="500" class="mt-12 mt-sm-0 pa-5 pa-lg-7">
+      <VCard
+        flat
+        :max-width="500"
+        class="mt-12 mt-sm-0 pa-5 pa-lg-7"
+      >
         <VCardText>
           <NuxtLink to="/">
             <div class="auth-logo app-logo">
@@ -240,7 +285,10 @@ const fetchType = ref([]);
         <VCardText>
           <h4 class="text-h4 mb-1">
             Buat Akun
-            <span class="text-capitalize" color="#652672">{{
+            <span
+              class="text-capitalize"
+              color="#652672"
+            >{{
               themeConfig.app.title
             }}</span>
           </h4>
@@ -250,9 +298,15 @@ const fetchType = ref([]);
         </VCardText>
 
         <VCardText>
-          <VForm ref="refVForm" @submit.prevent="onSubmit">
+          <VForm
+            ref="refVForm"
+            @submit.prevent="onSubmit"
+          >
             <VRow>
-              <VCol cols="12" style="max-block-size: 45svh; overflow-y: auto">
+              <VCol
+                cols="12"
+                style="max-block-size: 45svh; overflow-y: auto;"
+              >
                 <!-- Tipe Pengguna -->
                 <VCol cols="12">
                   <b> Tipe Pengguna</b>
@@ -260,7 +314,6 @@ const fetchType = ref([]);
                   <VSelect
                     v-model="form.typeUser"
                     :items="fetchType"
-                    :rules="[requiredValidator]"
                     item-title="name"
                     item-value="id"
                     label="Pilih tipe pengguna"
@@ -277,10 +330,19 @@ const fetchType = ref([]);
                     v-model="form.name"
                     placeholder="Masukan Nama"
                     type="text"
-                    :rules="[requiredValidator]"
-                    :error-messages="errors.name"
-                    @blur="validateName"
+                    @input="validateName"
                   />
+
+                  <span
+                    v-if="errors.name"
+                    class="error-text"
+                  >
+                    <VIcon
+                      icon="mdi-alert-circle"
+                      color="error"
+                      size="small"
+                    />  {{ errors.name }}
+                  </span>
                 </VCol>
 
                 <!-- email -->
@@ -288,45 +350,83 @@ const fetchType = ref([]);
                   <b> Email </b>
                   <VTextField
                     v-model="form.email"
-                    :rules="[
-                      requiredValidator,
-                      emailValidator,
-                      requiredValidasiEmail,
-                    ]"
                     type="text"
-                    :error-messages="errors.email"
                     placeholder="Masukan Email"
+                    @input="validateEmail"
                   />
+                  <span
+                    v-if="errors.email"
+                    class="error-text"
+                  >
+                    <VIcon
+                      icon="mdi-alert-circle"
+                      color="error"
+                      size="small"
+                    />  {{ errors.email }}
+                  </span>
                 </VCol>
+
+                <!-- :error-messages="errors.email" -->
+                <!--
+                  :rules="[
+                  requiredValidator,
+                  emailValidator,
+                  requiredValidasiEmail,
+                  ]"
+                -->
 
                 <!-- no Handphone -->
                 <VCol cols="12">
                   <b> Nomor Handphone</b>
                   <VTextField
                     v-model="form.noHandphone"
-                    :rules="[requiredValidator, requiredValidasinoHP]"
                     type="tel"
                     maxlength="13"
                     placeholder="Masukan Nomor Handphone"
-                    :error-messages="errors.noHandphone"
-                    @input="onlyAcceptNumber"
+                    @input="validateNomorHandphone"
                   />
+                  <span
+                    v-if="errors.noHandphone"
+                    class="error-text"
+                  >
+                    <VIcon
+                      icon="mdi-alert-circle"
+                      color="error"
+                      size="small"
+                    />  {{ errors.noHandphone }}
+                  </span>
                 </VCol>
+                <!-- :rules="[requiredValidator, requiredValidasinoHP]" -->
+                <!--
+                  :error-messages="errors.noHandphone"
+                  @input="onlyAcceptNumber"
+                -->
 
                 <!-- password -->
                 <VCol cols="12">
                   <b> Kata Sandi</b>
                   <VTextField
                     v-model="form.password"
-                    :rules="[requiredValidator, requiredMinLength]"
                     :type="isPasswordVisible ? 'text' : 'password'"
-                    :error-messages="errors.password"
                     :append-inner-icon="
                       isPasswordVisible ? 'ri-eye-off-line' : 'ri-eye-line'
                     "
                     placeholder="Masukan kata sandi"
                     @click:append-inner="isPasswordVisible = !isPasswordVisible"
+                    @input="validatePassword"
                   />
+                  <!-- :error-messages="errors.password" -->
+                  <!-- :rules="[requiredValidator, requiredMinLength]" -->
+                  <span
+                    v-if="errors.password"
+                    class="error-text"
+                  >
+                    <VIcon
+                      icon="mdi-alert-circle"
+                      color="error"
+                      size="small"
+                    />  {{ errors.password }}
+                  </span>
                 </VCol>
 
                 <!-- password confrim -->
@@ -334,13 +434,7 @@ const fetchType = ref([]);
                   <b>Konfirmasi Kata Sandi</b>
                   <VTextField
                     v-model="form.passwordConfirm"
-                    :rules="[
-                      requiredValidator,
-                      requiredMinLength,
-                      requiredSamePassword,
-                    ]"
                     :type="isPasswordVisibleConfrim ? 'text' : 'password'"
-                    :error-messages="errors.password"
                     :append-inner-icon="
                       isPasswordVisibleConfrim
                         ? 'ri-eye-off-line'
@@ -350,19 +444,45 @@ const fetchType = ref([]);
                     @click:append-inner="
                       isPasswordVisibleConfrim = !isPasswordVisibleConfrim
                     "
+                    @input="validateConfrimPassword"
                   />
+                  <!-- :error-messages="errors.password" -->
+                  <!--
+                    :rules="[
+                    requiredValidator,
+                    requiredMinLength,
+                    requiredSamePassword,
+                    ]"
+                  -->
+                  <span
+                    v-if="errors.passwordConfirm"
+                    class="error-text"
+                  >
+                    <VIcon
+                      icon="mdi-alert-circle"
+                      color="error"
+                      size="small"
+                    />  {{ errors.passwordConfirm }}
+                  </span>
                 </VCol>
               </VCol>
 
               <VCol cols="12">
-                <VBtn block :disabled="isDisabledSubmit" type="submit">
+                <VBtn
+                  block
+                  :disabled="isDisabledSubmit"
+                  type="submit"
+                >
                   Buat Akun
                 </VBtn>
               </VCol>
               <!-- disabled -->
 
               <!-- create account -->
-              <VCol cols="12" class="text-body-1 text-center">
+              <VCol
+                cols="12"
+                class="text-body-1 text-center"
+              >
                 <span class="d-inline-block"> Sudah punya akun ?</span>
                 <NuxtLink
                   class="text-primary ms-1 d-inline-block text-body-1"
@@ -390,15 +510,37 @@ const fetchType = ref([]);
           </VForm>
         </VCardText>
         <VCardText>
-          <VCol cols="12" class="text-body-1 text-center">
+          <VCol
+            cols="12"
+            class="text-body-1 text-center"
+          >
             <span class="d-inline-block">Terhubung Ke</span>
           </VCol>
-          <VRow align="center" justify="center">
-            <VCol cols="12" md="auto" class="d-flex align-center">
-              <VImg :src="ossImage" width="100" height="48" />
+          <VRow
+            align="center"
+            justify="center"
+          >
+            <VCol
+              cols="12"
+              md="auto"
+              class="d-flex align-center"
+            >
+              <VImg
+                :src="ossImage"
+                width="100"
+                height="48"
+              />
             </VCol>
-            <VCol cols="12" md="auto" class="d-flex align-center">
-              <VImg :src="bseImage" width="100" height="48" />
+            <VCol
+              cols="12"
+              md="auto"
+              class="d-flex align-center"
+            >
+              <VImg
+                :src="bseImage"
+                width="100"
+                height="48"
+              />
             </VCol>
           </VRow>
         </VCardText>
@@ -429,5 +571,12 @@ const fetchType = ref([]);
 
 .bg-white {
   background-color: white;
+}
+
+.error-text {
+  display: block;
+  color: red;
+  font-size: 0.875rem;
+  margin-block-start: 4px;
 }
 </style>
