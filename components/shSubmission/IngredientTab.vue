4487ccab97ab9fa07586fc6b3938fcc0959f7449
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2024-11-28 04:36:19 +0700

    adjust styling & data on 'bahan' tab

--- components/shSubmission/IngredientTab.vue
+++ components/shSubmission/IngredientTab.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-const deleteDialogVisible = ref(false);
-const dialogVisible = ref(false);
-
-const productHeader: any = [
+const ingredientHeader: any = [
   { title: "No", value: "index" },
   { title: "ID", value: "id", nowrap: true },
   { title: "Jenis Bahan", value: "type", nowrap: true },
@@ -31,7 +28,7 @@ const productHeader: any = [
     align: "center",
   },
 ];
-const productData = ref([
+const ingredientData = ref([
   {
     id: "89379868",
     type: "Kopi Luak Ciater",
@@ -41,46 +38,55 @@ const productData = ref([
     producer: "Luakmantuy",
     certificateNumber: "72836/Halal/V",
     effectiveDate: "12/09/2023",
-    isVerified: true,
+    isVerified: false,
   },
 ]);
 
 const verifiedProduct = computed(() => {
   let count = 0;
-  productData.value.forEach((item) => {
+  ingredientData.value.forEach((item) => {
     if (item.isVerified) count++;
   });
   return count;
 });
 
+const isFormModalOpen = ref(false);
+const isDeleteModalOpen = ref(false);
 const selectedDelete = ref();
-const handleOpenDeleteDialog = (index: number) => {
-  selectedDelete.value = index;
-  deleteDialogVisible.value = true;
-};
-const confirmDelete = () => {
-  const exist = productData.value.findIndex(
-    (i, idx) => idx === selectedDelete.value
-  );
-  productData.value.splice(exist, 1);
-  selectedDelete.value = null;
+
+const handleSaveVerified = () => {
+  useSnackbar().sendSnackbar("Berhasil menyimpan verifikasi data", "success");
 };
 
-const handleOpenDialog = () => {
-  dialogVisible.value = !dialogVisible.value;
+const handleOpenDeleteModal = (index: number) => {
+  selectedDelete.value = index;
+  isDeleteModalOpen.value = true;
+};
+const handleDeleteIngredient = () => {
+  const exist = ingredientData.value.findIndex(
+    (i, idx) => idx === selectedDelete.value
+  );
+  ingredientData.value.splice(exist, 1);
+  selectedDelete.value = null;
+  useSnackbar().sendSnackbar("Berhasil menghapus data", "success");
 };
 </script>
 
 <template>
   <VCard>
-    <VCardTitle class="my-5 d-flex justify-space-between align-center">
+    <VCardTitle class="my-3 d-flex justify-space-between align-center">
       <div class="font-weight-bold text-h4">Daftar Nama Bahan dan Kemasan</div>
       <div>
-        <VBtn variant="outlined" class="me-3" @click="handleOpenDialog">
+        <VBtn variant="outlined" class="me-3">
           <div class="pe-3">Tambah</div>
           <VIcon icon="fa-plus" />
         </VBtn>
-        <VBtn variant="flat" :color="verifiedProduct ? 'primary' : `#A09BA1`">
+        <VBtn
+          variant="flat"
+          :color="verifiedProduct ? 'primary' : `#A09BA1`"
+          :disabled="verifiedProduct == 0"
+          @click="handleSaveVerified"
+        >
           <div class="pe-3">
             {{
               verifiedProduct
@@ -94,10 +100,11 @@ const handleOpenDialog = () => {
     </VCardTitle>
     <VCardText>
       <VDataTable
-        :headers="productHeader"
-        :items="productData"
+        class="ingredient-table"
+        :headers="ingredientHeader"
+        :items="ingredientData"
         fixed-header
-        hide-default-footer
+        :hide-default-footer="ingredientData.length < 10"
       >
         <template #item.index="{ index }">
           {{ index + 1 }}
@@ -118,7 +125,7 @@ const handleOpenDialog = () => {
             </template>
             <VList>
               <VListItem prepend-icon="mdi-pencil" title="Ubah" />
-              <VListItem @click="handleOpenDeleteDialog(props.index)">
+              <VListItem @click="handleOpenDeleteModal(props.index)">
                 <template #prepend>
                   <VIcon icon="mdi-delete" color="error" />
                 </template>
@@ -132,10 +139,10 @@ const handleOpenDialog = () => {
   </VCard>
   <ShSubmissionDetailFormModal
     dialog-title="Menghapus Data"
-    :dialog-visible="deleteDialogVisible"
+    :dialog-visible="isDeleteModalOpen"
     dialog-use="DELETE"
-    @update:dialog-visible="deleteDialogVisible = $event"
-    @submit:commit-action="confirmDelete"
+    @update:dialog-visible="isDeleteModalOpen = $event"
+    @submit:commit-action="handleDeleteIngredient"
   >
     <VCardText>
       <div>Apakah yakin ingin menghapus data ini</div>
@@ -143,9 +150,25 @@ const handleOpenDialog = () => {
   </ShSubmissionDetailFormModal>
   <ShSubmissionDetailUploadModal
     dialog-title="Tambah Data Bahan"
-    :dialog-visible="dialogVisible"
-    @update:dialog-visible="dialogVisible = $event"
+    :dialog-visible="isFormModalOpen"
+    @update:dialog-visible="isFormModalOpen = $event"
   />
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+:deep(.v-data-table.ingredient-table > .v-table__wrapper) {
+  table {
+    thead > tr > th:last-of-type {
+      right: 0;
+      position: sticky;
+      border-left: 1px solid rgba(#000000, 0.12);
+    }
+    tbody > tr > td:last-of-type {
+      right: 0;
+      position: sticky;
+      border-left: 1px solid rgba(#000000, 0.12);
+      background: white;
+    }
+  }
+}
+</style>
