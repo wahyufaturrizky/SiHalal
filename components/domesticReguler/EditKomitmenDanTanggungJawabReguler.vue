52372c56390ebf38ac1e0de0951a7c864faaf76b
amevide998 <hdsvidi@gmaill.com>
2024-11-27 13:41:21 +0700

    add check list for komitmen dan kebijakan

--- components/domesticReguler/EditKomitmenDanTanggungJawabReguler.vue
+++ components/domesticReguler/EditKomitmenDanTanggungJawabReguler.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 
+
+// TODO -> PERLU HIT API, APAKAH SUDAH MENYETUJUI KEBIJAKAN HALAL
+const isAgree = ref(false)
+
+const confirm = check => isAgree.value = check
+
 </script>
 
 <template>
-  <FormKomitmenDanTanggungJawab />
+  <FormChecklistKomitmenDanTanggungJawab v-if="!isAgree" @confirm="confirm"/>
+  <FormKomitmenDanTanggungJawab v-if="isAgree"/>
 </template>
 
 <style scoped lang="scss">
