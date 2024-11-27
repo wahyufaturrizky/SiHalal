776a315bfd4e660a73e20c98cf4b9dc3e5f6e5e3
wahyu.rizki <wahyu.rizki@accenture.com>
2024-11-27 17:29:16 +0700

    FEAT: Lembaga

---
+++ server/api/facilitate/lembaga/list/[id].get.ts
@@ -0,0 +1,45 @@
+import type { NuxtError } from "nuxt/app";
+
+const runtimeConfig = useRuntimeConfig();
+export default defineEventHandler(async (event) => {
+  const authorizationHeader = getRequestHeader(event, "Authorization");
+  if (typeof authorizationHeader === "undefined") {
+    throw createError({
+      statusCode: 403,
+      statusMessage:
+        "Need to pass valid Bearer-authorization header to access this endpoint",
+    });
+  }
+
+  const id = getRouterParam(event, "id");
+
+  const { page, size, keyword } = (await getQuery(event)) as {
+    page: string;
+    size: string;
+    keyword: string;
+  };
+
+  const params = {
+    page: isNaN(Number.parseInt(page, 10)) ? 1 : Number.parseInt(page, 10),
+    size: isNaN(Number.parseInt(size, 10)) ? 10 : Number.parseInt(size, 10),
+  };
+
+  if (keyword != "") {
+  }
+  params["keyword"] = keyword;
+
+  const data = await $fetch<any>(
+    `${runtimeConfig.coreBaseUrl}/api/v1/fasilitator/lembaga/${id}/list`,
+    {
+      method: "get",
+      headers: { Authorization: authorizationHeader },
+      params,
+    }
+  ).catch((err: NuxtError) => {
+    setResponseStatus(event, 400);
+
+    return err.data;
+  });
+
+  return data || null;
+});
