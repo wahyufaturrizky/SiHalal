9684fdc1522e19a6467824c20a3a3eb90bb424a6
Farezas <fareza087@gmail.com>
2024-11-30 18:25:15 +0700

    feature: SH edit pemantauan dan evaluasi reguler

---
+++ components/domesticReguler/pemantauanComponent/RisalahKajiUlang.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const tableHeader = [
+  { title: "No", value: "no" },
+  { title: "Materi", value: "material" },
+  { title: "Hasil Pembahasan", value: "discussion_result" },
+];
+
+const items = [
+  {
+    material: "xx",
+    discussion_result: "xx",
+  },
+];
+</script>
+<template>
+  <VCard>
+    <VCardTitle>
+      <VRow>
+        <VCol cols="6"><h3>Risalah Kaji Ulang Manajemen</h3></VCol>
+      </VRow>
+    </VCardTitle>
+    <VCardItem>
+      <VDataTable :headers="tableHeader" :items="items"></VDataTable>
+    </VCardItem>
+  </VCard>
+</template>
