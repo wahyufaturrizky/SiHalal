9dd760d0dcee9a4c48b7420f16e3417aad79b2bf
Farezas <fareza087@gmail.com>
2024-11-30 18:26:29 +0700

    feature: SH tab reguler edit dokumen

--- components/domesticReguler/EditDokumenReguler.vue
+++ components/domesticReguler/EditDokumenReguler.vue
@@ -1,13 +1,25 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    Unduh Dokumen kelengkapan
-  </div>
+  <VCard>
+    <VCardTitle><h3>Unduh Dokumen kelengkapan</h3></VCardTitle>
+    <VCardItem>
+      <VRow>
+        <VCol cols="4">Dokumen Surat Permohonan</VCol>
+        <VCol cols="1">:</VCol>
+        <VCol cols="7" style="display: flex; justify-content: start">
+          <VBtn append-icon="fa-download" variant="flat">Unduh</VBtn>
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol cols="4">Dokumen SJPH</VCol>
+        <VCol cols="1">:</VCol>
+        <VCol cols="7" style="display: flex; justify-content: start">
+          <VBtn append-icon="fa-download" variant="flat">Unduh</VBtn>
+        </VCol>
+      </VRow>
+    </VCardItem>
+  </VCard>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
