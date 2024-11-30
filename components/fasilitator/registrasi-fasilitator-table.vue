07678c969f491834884467c42e5ba9974e70377a
geryn25 <35487884+geryn25@users.noreply.github.com>
2024-11-30 18:40:55 +0700

    Update registrasi-fasilitator-table.vue

--- components/fasilitator/registrasi-fasilitator-table.vue
+++ components/fasilitator/registrasi-fasilitator-table.vue
@@ -1,16 +1,68 @@
 <script setup lang="ts">
-import { VDataTable } from "vuetify/components";
-
 const tableHeader = [
-  { title: "No", value: "no" },
-  { title: "Kode Fasilitasi", value: "fac_code" },
-  { title: "Tahun", value: "fac_year" },
+  { title: "No", value: "index" },
+  { title: "Kode Fasilitasi", value: "kode_fac" },
+  { title: "Tahun", value: "tahun" },
   { title: "Nama Fasilitasi", value: "fac_name" },
-  { title: "Sumber Pembiayaan", value: "fac_sof" },
-  { title: "Jenis", value: "fac_kind" },
+  { title: "Sumber Pembiayaan", value: "sumber_biaya" },
+  { title: "Jenis", value: "jenis" },
+  { title: "Action", value: "action" },
 ];
+const itemPerPage = ref(10);
+const totalItems = ref(0);
+const loading = ref(true);
+const page = ref(1);
+const items = ref([]);
+const loadItem = async (page: number, size: number) => {
+  try {
+    loading.value = true;
+
+    const response = await $api("/facilitate/entry", {
+      method: "get",
+      params: {
+        page,
+        size,
+        keyword: "",
+      },
+    });
+
+    items.value = response.data;
+    totalItems.value = response.total_item;
+    loading.value = false;
+  } catch (error) {
+    useSnackbar().sendSnackbar("Ada Kesalahan", "error");
+    loading.value = false;
+  }
+};
+const navigateAction = (id: string) => {
+  navigateTo(`/facilitation/entry/${id}`);
+};
 </script>
 
 <template>
-  <VDataTable :headers="tableHeader"></VDataTable>
+  <VDataTableServer
+    :headers="tableHeader"
+    v-model:items-per-page="itemPerPage"
+    v-model:page="page"
+    :items-length="totalItems"
+    :loading="loading"
+    loading-text="Loading..."
+    :items="items"
+    @update:options="loadItem(page, itemPerPage)"
+  >
+    <template #item.index="{ index }">
+      {{ index + 1 + (page - 1) * itemPerPage }}
+    </template>
+    <template #item.action="{ item }">
+      <div class="d-flex gap-1">
+        <IconBtn size="small" @click="">
+          <VIcon
+            icon="ri-arrow-right-line"
+            color="primary"
+            @click="navigateAction(item.id)"
+          />
+        </IconBtn>
+      </div>
+    </template>
+  </VDataTableServer>
 </template>
