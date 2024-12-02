0e019f2f432e86547b1649d834c3ea414e415358
amevide998 <hdsvidi@gmaill.com>
2024-12-02 23:27:32 +0700

    add tab proses pengajuan reguler

---
+++ components/form/FormCatatanDistribusi.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+
+import FormEditCatatanDistribusi from "@/components/form/FormEditCatatanDistribusi.vue"
+import FormEditLayoutProduksi from "@/components/form/FormEditLayoutProduksi.vue"
+import FormTambahCatatanDistribusi from "@/components/form/FormTambahCatatanDistribusi.vue"
+import FormTambahLayoutProduksi from "@/components/form/FormTambahLayoutProduksi.vue"
+
+const snackBar = useSnackbar()
+
+const props = defineProps({
+  isEditable: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const catatanHeaders = [
+  { title: "No", key: "no" },
+  { title: "Nama Catatan", key: "name" , nowrap: true},
+  { title: "Tipe Penambahan", key: "type" , nowrap: true},
+  { title: "Jumlah", key: "total" , nowrap: true},
+  { title: "Tanggal Distribusi", key: "distributionate" , nowrap: true},
+  { title: "Tujuan", key: "destination" , nowrap: true},
+  { title: "File Dokumen", key: "file" , value: "file", sortable: false, nowrap: true},
+  { title: "Action", value: "action", sortable: false, nowrap: true },
+];
+
+const catatanItems = [
+  {
+    no: 1,
+    name: "Catatan 1",
+    type: 'Unggah Foto',
+    total: '-',
+    distributionate: '12/11/2024',
+    destination: 'Palembang',
+    file: null
+  },
+  {
+    no: 2,
+    name: "Catatan 1",
+    type: 'Unggah Foto',
+    total: '-',
+    distributionate: '12/11/2024',
+    destination: 'Palembang',
+    file: null
+  },
+];
+
+
+
+// TODO -> LOGIC DELETE BY ID
+const remove = (no) => {
+  console.log("remove BY ID : ", no);
+  snackBar.sendSnackbar("Berhasil menghapus data", "success");
+};
+
+// TODO -> LOGIC EDIT BY ID
+const update = (form) => {
+  console.log("update : ", form);
+  snackBar.sendSnackbar("Berhasil menambahkan data", "success")
+};
+
+// TODO -> LOGIC TAMBAH DATA
+const save = (form) => {
+  console.log("save ", form);
+  snackBar.sendSnackbar("Berhasil menambahkan data", "success")
+};
+
+// TODO -> LOGIC TAMBAH DATA
+const upload = (file) => {
+  console.log("upload ", file);
+  snackBar.sendSnackbar("Berhasil menambahkan data", "success")
+};
+
+// TODO -> LOGIC TO DONWLOAD FILE
+const download = item => console.log("download ", item)
+
+</script>
+
+<template>
+  <VCard class="pa-4 mb-8">
+    <VCardTitle class="d-flex justify-space-between align-center">
+      <span class="text-h3">Catatan Distribusi / Penjualan Produk </span>
+      <FormTambahCatatanDistribusi :is-editable="props.isEditable" @confirm="save"  @upload="upload"/>
+    </VCardTitle>
+    <VCardItem>
+      <VDataTable :headers="catatanHeaders" :items="catatanItems">
+        <template #item.action="{ item }">
+          <v-btn color="primary" variant="plain">
+            <VIcon>mdi-dots-vertical</VIcon>
+            <VMenu activator="parent" :close-on-content-click="false">
+              <VCard>
+                <FormEditCatatanDistribusi
+                    :is-editable="props.isEditable"
+                    :initial-data="item"
+                    @confirm="update"
+                />
+                <VBtn
+                    :disabled="!props.isEditable"
+                    variant="text"
+                    color="error"
+                    prepend-icon="ri-delete-bin-6-line"
+                    @click="remove(item.no)"
+                    block
+                >
+                  Hapus
+                </VBtn>
+              </VCard>
+            </VMenu>
+          </v-btn>
+        </template>
+        <template #item.file="{ item }">
+          <v-btn color="primary" variant="plain" prepend-icon="mdi-download" @click="download(item)">
+            File
+          </v-btn>
+        </template>
+      </VDataTable>
+    </VCardItem>
+  </VCard>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ components/form/FormEditCatatanHasilProduksi.vue
@@ -0,0 +1,196 @@
+<script setup lang="ts">
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import { useDisplay } from "vuetify";
+
+const props = defineProps({
+  initialData: { type : Object, default: null},
+  isEditable: { type: Boolean, default: true },
+});
+
+const emit = defineEmits(["confirm", "upload"]);
+
+const snackbar = useSnackbar();
+
+const tabs = ref(0);
+const file = ref(null);
+
+onMounted(() => {
+  tabs.value = 0;
+});
+
+const isVisible = ref(false);
+const openDialog = () => isVisible.value = true
+const closeDialog = () => isVisible.value = false
+
+
+const catatan = ref(props.initialData)
+
+
+const unggahData = () => {
+  console.log("FILE CATATAN {}", file.value)
+  emit("upload", file.value);
+  closeDialog();
+  file.value = null;
+  snackbar.sendSnackbar("Berhasil menambahkan data", "success");
+};
+
+const confirm = () => {
+  console.log("MANUAL CATATAN ", catatan.value)
+  emit("confirm", catatan.value);
+  closeDialog()
+  snackbar.sendSnackbar("Berhasil menambahkan data", "success");
+};
+
+const { mdAndUp } = useDisplay();
+
+const dialogMaxWidth = computed(() => {
+  return mdAndUp.value ? 700 : "90%";
+});
+
+
+</script>
+
+<template>
+  <VBtn
+    variant="text"
+    prepend-icon="ri-edit-line"
+    @click="openDialog"
+    block
+    :disabled="!props.isEditable"
+  >
+    Ubah
+  </VBtn>
+  <VDialog v-model="isVisible" :max-width="dialogMaxWidth">
+    <VCard class="pa-2">
+      <VCardTitle
+        class="text-h5 font-weight-bold d-flex justify-space-between align-center"
+      >
+        <span> Ubah Catatan Hasil Produksi</span>
+        <VBtn
+          icon
+          color="transparent"
+          style="border: none"
+          elevation="0"
+          @click="closeDialog"
+        >
+          <VIcon color="black"> ri-close-line </VIcon>
+        </VBtn>
+      </VCardTitle>
+      <VCardItem>
+        <div class="d-flex justify-center align-center">
+          <VTabs
+            v-model="tabs"
+            align-tabs="center"
+            bg-color="#f0dcf5"
+            class="border pa-2"
+            style="border-radius: 40px"
+            height="auto"
+          >
+            <VTab
+              value="1"
+              base-color="#f0dcf5"
+              active-color="primary"
+              style="border-radius: 40px"
+              hide-slider
+              color="primary"
+              variant="flat"
+              height="40px"
+            >
+              <span>Unggah File </span>
+            </VTab>
+            <VTab
+              value="2"
+              active-color="primary"
+              base-color="#f0dcf5"
+              style="border-radius: 40px"
+              hide-slider
+              variant="flat"
+              height="40px"
+            >
+              <span> Tambah Manual </span>
+            </VTab>
+          </VTabs>
+        </div>
+        <VTabsWindow v-model="tabs">
+          <br />
+          <VTabsWindowItem value="1">
+            <VRow no-gutters>
+              <VCol cols="6" class="d-flex align-center">
+                <span> Unggah Hasil </span>
+              </VCol>
+              <VCol cols="6">
+                <HalalFileInput v-model="file" label="Pilih File" />
+              </VCol>
+            </VRow>
+            <VCardActions class="d-flex justify-end ga-2 mt-4">
+              <VBtn
+                variant="outlined"
+                color="error"
+                min-width="100px"
+                @click="closeDialog"
+              >
+                Batal
+              </VBtn>
+              <VBtn variant="flat" min-width="120px" @click="unggahData">
+                Unggah
+              </VBtn>
+            </VCardActions>
+          </VTabsWindowItem>
+
+          <VTabsWindowItem value="2">
+            <VLabel for="nama"> Nama Produk </VLabel>
+            <VTextField id="nama"
+                        placeholder="Isi Nama Produk"
+                        v-model="catatan.name"
+                        class="mb-2"
+            />
+            <VLabel for="total"> Jumlah </VLabel>
+            <VTextField id="total"
+                        placeholder="Isi Jumlah"
+                        v-model="catatan.total"
+                        class="mb-2"
+            />
+            <VRow>
+              <VCol cols="6">
+                <VLabel for="productionDate"> Tanggal Produksi</VLabel>
+                <VueDatePicker
+                  class="mb-4"
+                  teleport-center
+                  id="productionDate"
+                  v-model="catatan.productionDate"
+                  :enable-time-picker="false"
+                />
+              </VCol>
+              <VCol cols="6">
+                <VLabel for="outDated"> Tanggal Kadaluarsa</VLabel>
+                <VueDatePicker
+                  class="mb-4"
+                  teleport-center
+                  id="outDated"
+                  v-model="catatan.outDated"
+                  :enable-time-picker="false"
+                />
+              </VCol>
+            </VRow>
+            <VCardActions class="d-flex justify-end ga-2 mt-4">
+              <VBtn
+                variant="outlined"
+                color="error"
+                min-width="100px"
+                @click="closeDialog"
+              >
+                Batal
+              </VBtn>
+              <VBtn variant="flat" min-width="120px" @click="confirm">
+                Ubah
+              </VBtn>
+            </VCardActions>
+          </VTabsWindowItem>
+        </VTabsWindow>
+      </VCardItem>
+    </VCard>
+  </VDialog>
+</template>
+
+<style scoped lang="scss"></style>
