874bd1cb413d7de18fa409fe0ec97715e9e7a04f
amevide998 <hdsvidi@gmaill.com>
2024-12-04 07:52:42 +0700

    create page tagihan pelaku usaha

---
+++ components/invoice/KonfirmasiPembayaran.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useDisplay } from "vuetify";
+
+const isVisible = ref(false);
+
+const openDialog = () => {
+  isVisible.value = true;
+};
+
+const closeDialog = () => {
+  isVisible.value = false;
+};
+
+const { mdAndUp } = useDisplay();
+
+const dialogMaxWidth = computed(() => {
+  return mdAndUp.value ? 700 : "90%";
+});
+</script>
+
+<template>
+  <VBtn
+    variant="text"
+    prepend-icon="mdi-check-all"
+    @click="openDialog"
+    block
+    class="text-left"
+    style="width: 100%; justify-content: flex-start;"
+  >
+    konfirmasi Pembayaran
+  </VBtn>
+  <VDialog v-model="isVisible" :max-width="dialogMaxWidth">
+    KONFIRMASI PEMBAYARAN
+  </VDialog>
+</template>
+
+<style scoped></style>
---
+++ components/invoice/LihatInvoice.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useDisplay } from "vuetify";
+
+const isVisible = ref(false);
+
+const openDialog = () => {
+  isVisible.value = true;
+};
+
+const closeDialog = () => {
+  isVisible.value = false;
+};
+
+const { mdAndUp } = useDisplay();
+
+const dialogMaxWidth = computed(() => {
+  return mdAndUp.value ? 700 : "90%";
+});
+</script>
+
+<template>
+  <VBtn
+    variant="text"
+    prepend-icon="mdi-download-box"
+    @click="openDialog"
+    block
+    class="text-left"
+    style="width: 100%; justify-content: flex-start;"
+  >
+    Lihat Invoice
+  </VBtn>
+  <VDialog v-model="isVisible" :max-width="dialogMaxWidth">
+    <VCard class="pa-8">
+      TODO - LIHAT INVOICE - PREVIEW PDF
+    </VCard>
+  </VDialog>
+</template>
+
+<style scoped></style>
