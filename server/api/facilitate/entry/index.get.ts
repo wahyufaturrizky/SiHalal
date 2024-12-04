354ca26e68cc6f9f45b422edb0b2cb80540d12b8
wahyu.rizki <wahyu.rizki@accenture.com>
2024-12-04 15:58:53 +0700

    FIX: status draft

--- server/api/facilitate/entry/index.get.ts
+++ server/api/facilitate/entry/index.get.ts
@@ -11,20 +11,19 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const { page, size, keyword } = (await getQuery(event)) as {
+  const { page, size, keyword, status } = (await getQuery(event)) as {
     page: string;
     size: string;
     keyword: string;
+    status: string;
   };
 
   const params = {
     page: isNaN(Number.parseInt(page, 10)) ? 1 : Number.parseInt(page, 10),
     size: isNaN(Number.parseInt(size, 10)) ? 10 : Number.parseInt(size, 10),
+    status,
+    keyword,
   };
-
-  if (keyword != "") {
-  }
-  params["keyword"] = keyword;
 
   const data = await $fetch<any>(
     `${runtimeConfig.coreBaseUrl}/api/v1/fasilitator/search`,
