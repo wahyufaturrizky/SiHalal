13640eed94e1f99f19c11c186dd740aec4841453
geryn25 <35487884+geryn25@users.noreply.github.com>
2024-12-04 10:39:52 +0700

    revert to turnstile

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,3 +1,4 @@
+import { TurnstileValidationResponse } from "@nuxtjs/turnstile/runtime/types.js";
 import { NuxtError } from "nuxt/app";
 
 const runtimeConfig = useRuntimeConfig();
@@ -12,34 +13,15 @@ export default defineEventHandler(async (event) => {
       },
     });
   }
-  console.log({
-    secret: runtimeConfig.recaptcha.secretKey,
-    response: token,
-  });
-  const recaptcha = await $fetch<any>(
-    `https://www.google.com/recaptcha/api/siteverify`,
-    {
-      method: "POST",
-      params: {
-        secret: runtimeConfig.recaptcha.secretKey,
-        response: token,
-      },
-    }
-  ).catch((err: NuxtError) => {
+  const turnstile = await $fetch<TurnstileValidationResponse>(
+    `${runtimeConfig.public.apiBaseUrl}/validateTurnstile`,
+    { method: "POST", body: { token: token } }
+  );
+  console.log(turnstile);
+  if (!turnstile.success) {
     throw createError({
       statusCode: 401,
       statusMessage: "captcha-failed",
-      data: err.data,
-    });
-  });
-  console.log(recaptcha);
-  if (!recaptcha.success) {
-    throw createError({
-      statusCode: 401,
-      statusMessage: "captcha-failed",
-      data: {
-        success: false,
-      },
     });
   }
   const { data } = await $fetch<any>(
