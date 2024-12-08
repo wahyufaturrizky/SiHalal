35e3d02b04dba04902d28db0f1447243c1a71dfd
geryn25 <35487884+geryn25@users.noreply.github.com>
2024-12-08 18:23:44 +0700

    update verifikator fasilitator list and detail

---
+++ server/api/facilitate/verifikator/return.post.ts
@@ -0,0 +1,30 @@
+import { NuxtError } from "nuxt/app";
+const runtimeConfig = useRuntimeConfig();
+export default defineEventHandler(async (event) => {
+  const authorizationHeader = getRequestHeader(event, "Authorization");
+  if (typeof authorizationHeader === "undefined") {
+    throw createError({
+      statusCode: 403,
+      statusMessage:
+        "Need to pass valid Bearer-authorization header to access this endpoint",
+    });
+  }
+  const { id, comment } = await readBody(event);
+
+  const data = await $fetch<any>(
+    `${runtimeConfig.coreBaseUrl}/api/v1/fasilitator/fasilitasi/return`,
+    {
+      method: "get",
+      headers: { Authorization: authorizationHeader },
+      body: {
+        fasilitasi_id: id,
+        keterangan: comment,
+      },
+    }
+  ).catch((err: NuxtError) => {
+    console.log(err);
+    setResponseStatus(event, 400);
+    return err.data;
+  });
+  return data || null;
+});
