3a207d02d9f22fdbfc0c2192ded81b2aeab32a23
Farezas <fareza087@gmail.com>
2024-12-12 13:32:34 +0700

    feature: modal tracking shln
    1. create modal for tracking shln
    2. modify tracking shln component props

---
+++ components/verificator/shlnSubmissionDetail/ShlnDocumentDetail/TrackingModal.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const isVisible = ref(false);
+</script>
+<template>
+  <IconBtn @click="isVisible = true">
+    <VIcon icon="fa-history" />
+  </IconBtn>
+  <VDialog v-model="isVisible">
+    <template #default>
+      <TrackingShln
+        :use-title="true"
+        title-text="Tracking Details"
+        :data="[]"
+      ></TrackingShln>
+    </template>
+  </VDialog>
+</template>
