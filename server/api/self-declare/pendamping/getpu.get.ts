3701b4dbcfc6a31f1326529d21a8e9919d1a59d3
Farezas <fareza087@gmail.com>
2024-12-22 19:38:41 +0700

    feature: penganjuan pu pendamping self declare

---
+++ server/api/self-declare/pendamping/getpu.get.ts
@@ -0,0 +1,45 @@
+import type { NuxtError } from "nuxt/app";
+
+const runtimeConfig = useRuntimeConfig();
+export default defineEventHandler(async (event) => {
+  const authorizationHeader = getRequestHeader(event, "Authorization");
+  if (typeof authorizationHeader === "undefined") {
+    throw createError({
+      statusCode: 403,
+      statusMessage:
+        "Need to pass valid Bearer-authorization header to access this endpoint",
+    });
+  }
+
+  const { page, size, keyword, query_by } = (await getQuery(event)) as {
+    page: string;
+    size: string;
+    keyword: string;
+    query_by: string;
+  };
+
+  const params: any = {
+    page: isNaN(Number.parseInt(page, 10)) ? 1 : Number.parseInt(page, 10),
+    size: isNaN(Number.parseInt(size, 10)) ? 10 : Number.parseInt(size, 10),
+    query_by,
+  };
+
+  if (keyword != "") {
+  }
+  params["keyword"] = keyword;
+
+  const data = await $fetch<any>(
+    `${runtimeConfig.coreBaseUrl}/api/v1/pendamping/halal-certificate-reguler/pengajuan`,
+    {
+      method: "get",
+      headers: { Authorization: authorizationHeader },
+      params,
+    }
+  ).catch((err: NuxtError) => {
+    setResponseStatus(event, 400);
+
+    return err.data;
+  });
+
+  return data || null;
+});
