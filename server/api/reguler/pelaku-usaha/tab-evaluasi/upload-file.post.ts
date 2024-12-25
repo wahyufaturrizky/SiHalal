c20daae4e521113317c4169aa0a2fe49e3f5c9a0
amevide998 <hdsvidi@gmaill.com>
2024-12-25 10:37:23 +0700

    add fitur pelaku usaha pemantauan dan evaluasi

---
+++ server/api/reguler/pelaku-usaha/tab-evaluasi/upload-file.post.ts
@@ -0,0 +1,35 @@
+import type { NuxtError } from 'nuxt/app'
+
+const runtimeConfig = useRuntimeConfig()
+export default defineEventHandler(async (event: any) => {
+  const authorizationHeader = getRequestHeader(event, 'Authorization')
+  if (typeof authorizationHeader === 'undefined') {
+    throw createError({
+      statusCode: 403,
+      statusMessage:
+        'Need to pass valid Bearer-authorization header to access this endpoint',
+    })
+  }
+  const form = await readFormData(event)
+
+  console.log('FORM DATA : ', form)
+
+  const { id } = (await getQuery(event)) as {
+    id: string
+  }
+
+  const data = await $fetch<any>(
+    `${runtimeConfig.coreBaseUrl}/api/importer/${id}/upload/documents`,
+    {
+      method: 'post',
+      headers: { Authorization: authorizationHeader },
+      body: form,
+    },
+  ).catch((err: NuxtError) => {
+    setResponseStatus(event, 400)
+
+    return err.data
+  })
+
+  return data || null
+})
