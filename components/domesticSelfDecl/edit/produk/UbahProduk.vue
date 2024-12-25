b86d6f126896b4ca1b197db2899f968f5c1db236
Farezas <fareza087@gmail.com>
2024-12-25 14:57:22 +0700

    fix: on edit validation, add vform in ubah and tambah produk self declare

--- components/domesticSelfDecl/edit/produk/UbahProduk.vue
+++ components/domesticSelfDecl/edit/produk/UbahProduk.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type { VForm } from "vuetify/components";
+
 const propsOutside = defineProps({
   icon: { type: String, default: "fa-pencil" }, // Default to existing icon
   showLabel: { type: Boolean, default: true }, // Control label visibility
@@ -27,8 +29,17 @@ const resetForm = () => {
   formData.value.foto_produk = null;
 };
 
+const formUbahProduk = ref<VForm>();
+
 const handleEdit = () => {
-  emit("confirm-edit", formData.value, propsOutside.idProduk);
+  formUbahProduk.value?.validate().then(({ valid: isValid }) => {
+    if (isValid) {
+      emit("confirm-edit", formData.value, propsOutside.idProduk);
+      openCloseModal.value = false;
+    } else {
+      openCloseModal.value = true;
+    }
+  });
 };
 
 const handleDetailProduct = async (submissionId: string, productId: string) => {
@@ -48,7 +59,7 @@ const handleDetailProduct = async (submissionId: string, productId: string) => {
       // detailProduct.value = response.data;
       console.log("response detail = ", response.data);
       formData.value.product_grade = response.data?.klasifikasi;
-      formData.value.kode_rincian = response.data?.koderincian;
+      formData.value.kode_rincian = response.data?.koderincian_desc;
       formData.value.nama_produk = response.data?.nama;
       formData.value.merek = response.data?.merek;
       formData.value.foto_produk = null;
@@ -105,6 +116,8 @@ const uploadedFile = ref({
   file: null,
 });
 
+const openCloseModal = ref();
+
 const handleUploadFile = async (event: any) => {
   if (event?.target?.files.length) {
     const fileData = event.target.files[0];
@@ -148,7 +161,7 @@ const onOpenModal = async () => {
 </script>
 
 <template>
-  <VDialog max-width="60svw">
+  <VDialog max-width="60svw" v-model="openCloseModal">
     <template #activator="{ props }">
       <VListItem v-bind="props" @click="onOpenModal()"
         ><VListItemTitle>
@@ -177,83 +190,88 @@ const onOpenModal = async () => {
           </VRow>
         </VCardTitle>
         <VCardItem>
-          <VItemGroup>
-            <VLabel><b>Klasifikasi Produk</b></VLabel>
-            <VSelect
-              density="compact"
-              placeholder="Pilih Klasifikasi Produk"
-              v-model="formData.product_grade"
-              :items="listClassification"
-              item-title="name"
-              item-value="code"
-              v-on:update:model-value="handleListClassificationDetail"
-            ></VSelect>
-          </VItemGroup>
-          <br />
-          <VItemGroup>
-            <VLabel><b>Rincian Produk</b></VLabel>
-            <VSelect
-              density="compact"
-              placeholder="Pilih Rincian Produk"
-              v-model="formData.kode_rincian"
-              :items="listClassificationDetail"
-              item-title="name"
-              item-value="code"
-            ></VSelect>
-          </VItemGroup>
-          <br />
-          <VItemGroup>
-            <VLabel><b>Nama Produk</b></VLabel>
-            <VTextField
-              density="compact"
-              placeholder="Isi Nama Produk"
-              v-model="formData.nama_produk"
-            ></VTextField>
-          </VItemGroup>
-          <br />
-          <VItemGroup>
-            <VLabel><b>Merk</b></VLabel>
-            <VTextField
-              density="compact"
-              placeholder="Isi Merk"
-              v-model="formData.merek"
-            ></VTextField>
-          </VItemGroup>
-          <br />
-          <VItemGroup>
-            <VRow>
-              <VCol cols="6" style="display: flex; align-items: center">
-                <b>Upload Produk</b>
-              </VCol>
-              <VCol cols="6">
-                <VFileInput
-                  class="custom-file-input"
-                  density="compact"
-                  rounded="xl"
-                  label="No file choosen"
-                  max-width="400"
-                  prepend-icon=""
-                  @change="handleUploadFile"
-                >
-                  <template #append-inner>
-                    <VBtn rounded="s-0 e-xl" text="Choose" />
-                  </template>
-                </VFileInput>
-              </VCol>
-            </VRow>
-          </VItemGroup>
-          <br />
+          <VForm ref="formUbahProduk" @submit.prevent>
+            <VItemGroup>
+              <VLabel><b>Klasifikasi Produk</b></VLabel>
+              <VSelect
+                density="compact"
+                placeholder="Pilih Klasifikasi Produk"
+                v-model="formData.product_grade"
+                :items="listClassification"
+                item-title="name"
+                item-value="code"
+                v-on:update:model-value="handleListClassificationDetail"
+                :rules="[requiredValidator]"
+              ></VSelect>
+            </VItemGroup>
+            <br />
+            <VItemGroup>
+              <VLabel><b>Rincian Produk</b></VLabel>
+              <VSelect
+                density="compact"
+                placeholder="Pilih Rincian Produk"
+                v-model="formData.kode_rincian"
+                :items="listClassificationDetail"
+                item-title="name"
+                item-value="code"
+                :rules="[requiredValidator]"
+              ></VSelect>
+            </VItemGroup>
+            <br />
+            <VItemGroup>
+              <VLabel><b>Nama Produk</b></VLabel>
+              <VTextField
+                density="compact"
+                placeholder="Isi Nama Produk"
+                v-model="formData.nama_produk"
+                :rules="[requiredValidator]"
+              ></VTextField>
+            </VItemGroup>
+            <br />
+            <VItemGroup>
+              <VLabel><b>Merk</b></VLabel>
+              <VTextField
+                density="compact"
+                placeholder="Isi Merk"
+                v-model="formData.merek"
+                :rules="[requiredValidator]"
+              ></VTextField>
+            </VItemGroup>
+            <br />
+            <VItemGroup>
+              <VRow>
+                <VCol cols="6" style="display: flex; align-items: center">
+                  <b>Upload Produk</b>
+                </VCol>
+                <VCol cols="6">
+                  <VFileInput
+                    class="custom-file-input"
+                    density="compact"
+                    rounded="xl"
+                    label="No file choosen"
+                    max-width="400"
+                    prepend-icon=""
+                    @change="handleUploadFile"
+                    :rules="[
+                      requiredValidator,
+                      fileExtensionValidator,
+                      fileNameLengthValidator,
+                      fileSizeValidator,
+                    ]"
+                  >
+                    <template #append-inner>
+                      <VBtn rounded="s-0 e-xl" text="Choose" />
+                    </template>
+                  </VFileInput>
+                </VCol>
+              </VRow>
+            </VItemGroup>
+            <br />
+          </VForm>
         </VCardItem>
         <VCardActions>
           <VBtn variant="outlined" @click="isActive.value = false"> Batal</VBtn>
-          <VBtn
-            @click="
-              handleEdit();
-              isActive.value = false;
-            "
-            variant="flat"
-            >Ubah</VBtn
-          >
+          <VBtn @click="handleEdit()" variant="flat">Ubah</VBtn>
         </VCardActions>
       </VCard>
     </template>
