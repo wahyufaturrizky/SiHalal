aa0b5240210c9ff707e03583266ae3669d81e476
wahyu.rizki <wahyu.rizki@accenture.com>
2024-12-25 23:58:25 +0700

    feat: update API authorization header to use dynamic access token

--- utils/api.ts
+++ utils/api.ts
@@ -8,7 +8,7 @@ export const $api = $fetch.create({
     if (accessToken) {
       options.headers = {
         ...options.headers,
-        Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjYzMjI4MjQsInBheWxvYWQiOiJscGgxNyIsInN1YiI6ImZjNzIwOThkLTAxZTYtNDRkNi05ZGM0LTM4ZDUwYTUyYjEzZCIsInRva2VuX3V1aWQiOiJmZmU0NTBhMS01ZWVkLTQzZWQtYmNjNS1iMjY3ZTA3M2NlZDIifQ.2Zp70bQNfSdpuEX5T-XoOGoFJV8hTwws9Mtny9Y8N9EMezrvCIiKEKTO_ermaJH2c8_12iPgAnx13Aq-Q18haA`,
+        Authorization: `Bearer ${accessToken}`,
       };
     }
   },
