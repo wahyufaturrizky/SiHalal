349f67a6a70d8042292f67364ebc713be3955633
geryn25 <35487884+geryn25@users.noreply.github.com>
2024-12-26 22:36:37 +0700

    update produk

---
+++ components/domesticReguler/EditPemantauanDanEvaluasiRegulerVerifikator.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps({
+  isEditable: {
+    type: Boolean,
+    default: true,
+  },
+})
+</script>
+
+<template>
+  <VRow>
+    <VCol cols="12"><UploadDocumentVerifikator></UploadDocumentVerifikator></VCol>
+  </VRow>
+  <VRow>
+    <VCol cols="12"><SignagePemantauanVerifikator></SignagePemantauanVerifikator></VCol>
+  </VRow>
+  <VRow>
+    <VCol cols="12"><FormulirAuditInternal :is-editable="props.isEditable"></FormulirAuditInternal></VCol>
+  </VRow>
+  <VRow>
+    <VCol cols="12"><RisalahKajiUlang :is-editable="props.isEditable"></RisalahKajiUlang></VCol>
+  </VRow>
+</template>
+
+<style scoped lang="scss"></style>
