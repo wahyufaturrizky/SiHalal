1baf4b14bd184ff4d2745b8108140c4d61c580a5
geryn25 <35487884+geryn25@users.noreply.github.com>
2024-12-26 21:46:20 +0700

    reguler detail proses di verifikator

--- components/domesticReguler/EditProsesRegulerVerifikator.vue
+++ components/domesticReguler/EditProsesRegulerVerifikator.vue
@@ -9,16 +9,15 @@ const props = defineProps({
 // TODO -> PERLU HIT API, APAKAH SUDAH MENYETUJUI PERNYTAAN BEBAS BABI (default nya false)
 const isAgree = ref(false || !props.isEditable)
 
-const confirm = check => isAgree.value = check
 
 </script>
 
 <template>
     <FormLayoutProduksiVerifikator/>
-    <FormCatatanPenyimpananBahanDanProduk :is-editable="props.isEditable"/>
-    <FormDiagramAlurProsesProduksi :is-editable="props.isEditable"/>
-    <FormCatatanHasilProduksi :is-editable="props.isEditable"/>
-    <FormCatatanDistribusi :is-editable="props.isEditable"/>
+    <FormCatatanPenyimpananBahanDanProdukVerifikator/>
+    <FormDiagramAlurProsesProduksiVerifikator/>
+    <FormCatatanHasilProduksiVerifikator/>
+    <FormCatatanDistribusiVerifikator/>
 </template>
 
 <style scoped lang="scss">
---
+++ components/form/FormCatatanHasilProduksiVerifikator.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import FormEditLayoutProduksi from "@/components/form/FormEditLayoutProduksi.vue";
+import FormTambahLayoutProduksi from "@/components/form/FormTambahLayoutProduksi.vue";
+
+const catatanHeaders = [
+  { title: "No", key: "no" },
+  { title: "Nama Produk", key: "nama_produk", nowrap: true },
+  { title: "Tipe Penambahan", key: "tipe_penambahan", nowrap: true },
+  { title: "Jumlah", key: "jumlah", nowrap: true },
+  { title: "Tanggal Produksi", key: "tanggal_produksi", nowrap: true },
+  { title: "Tanggal Kadaluarsa", key: "tanggal_kadaluarsa", nowrap: true },
+  {
+    title: "File Dokumen",
+    key: "file",
+    value: "file",
+    sortable: false,
+    nowrap: true,
+  },
+];
+
+const catatanItems = ref([]);
+
+const route = useRoute();
+const getCatatanProduksi = async () => {
+  try {
+    const response = await $api("/reguler/verifikator/detail/proses/catatan-produksi", {
+      method: "post",
+      body: {
+        id_reg: route.params.id,
+      },
+    });
+    if (response.code != 2000) {
+      useSnackbar().sendSnackbar("ada kesalahan", "error");
+      return;
+    }
+    catatanItems.value = response.data;
+  } catch (error) {
+    useSnackbar().sendSnackbar("ada kesalahan", "error");
+  }
+};
+onMounted(async () => {
+  await getCatatanProduksi();
+});
+// TODO -> LOGIc DOWNLOAD
+const download = async (item) => {
+  await downloadDocument(item);
+};
+</script>
+
+<template>
+  <VCard class="pa-4 mb-8">
+    <VCardTitle class="d-flex justify-space-between align-center">
+      <span class="text-h3">Catatan Hasil Produksi </span>
+    </VCardTitle>
+    <VCardItem>
+      <VDataTable :headers="catatanHeaders" :items="catatanItems">
+        <template #item.no="{index}"> 
+          {{index + 1}}
+        </template>
+        <template #item.file="{ item }">
+          <v-btn
+          :disabled="item.file_dok == ''"
+            color="primary"
+            variant="plain"
+            prepend-icon="mdi-download"
+            @click="download(item)"
+          >
+            File
+          </v-btn>
+        </template>
+      </VDataTable>
+    </VCardItem>
+  </VCard>
+</template>
+
+<style scoped lang="scss"></style>
