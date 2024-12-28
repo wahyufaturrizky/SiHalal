7ca120cc7f80ba37af3ad6a77071af075e2830e1
adek ferdian <adekferdian@gmail.com>
2024-12-28 23:30:42 +0700

    fixing update biaya lph

---
+++ server/api/reguler/lph/update-cost/seed-detail.post.ts
@@ -0,0 +1,30 @@
+import type { NuxtError } from "nuxt/app";
+import type { NewAccountGovernment } from "~/server/interface/new-account.iface";
+
+const runtimeConfig = useRuntimeConfig();
+export default defineEventHandler(async (event) => {
+  const authorizationHeader = getRequestHeader(event, "Authorization");
+  if (typeof authorizationHeader === "undefined") {
+    throw createError({
+      statusCode: 403,
+      statusMessage:
+        "Need to pass valid Bearer-authorization header to access this endpoint",
+    });
+  }
+
+  const { id } = (await getQuery(event)) as {
+    id: string;
+  };
+
+  const data = await $fetch<any>(
+    `${runtimeConfig.coreBaseUrl}/api/v1/halal-certificate-reguler/lph/biaya/${id}/seed-detail`,
+    {
+      method: "post",
+      headers: { Authorization: authorizationHeader },
+    }
+  ).catch((err: NuxtError) => {
+    return err.data;
+  });
+
+  return data || null;
+});
