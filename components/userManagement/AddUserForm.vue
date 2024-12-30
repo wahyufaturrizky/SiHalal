be32c35f338a9f360ac1fb2f9239ff4cf307a07c
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2024-12-31 00:44:21 +0700

    Add text field for password on create user modal

--- components/userManagement/AddUserForm.vue
+++ components/userManagement/AddUserForm.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 const props = defineProps<{
   dialogVisible: boolean;
-  openHandler: Function;
+  closeHandler: Function;
 }>();
 
-const emit = defineEmits(["update:handleSubmit"]);
+const emit = defineEmits(["submit:add"]);
 
 const form = ref();
 const formData = reactive({
   username: null,
   name: null,
   email: null,
+  password: null,
   phone_number: null,
   is_verified: false,
   role: null,
@@ -19,6 +20,7 @@ const formRules = reactive({
   username: [(v: string) => !!v || "Username is required!"],
   name: [(v: string) => !!v || "Name is required!"],
   email: [(v: string) => !!v || "Email is required!"],
+  password: [(v: string) => !!v || "Password is required!"],
   phoneNumber: [(v: string) => !!v || "Phone number is required!"],
   role: [(v: string) => !!v || "Role is required!"],
 });
@@ -48,12 +50,15 @@ const handleResetForm = () => {
     role: null,
   });
 };
+const handleGeneratePassword = () => {
+  // generate password here
+};
 const handleSubmitForm = async () => {
   const status = await form.value.validate();
 
   if (status.valid) {
-    emit("update:handleSubmit", formData);
-    props.openHandler();
+    emit("submit:add", formData);
+    props.closeHandler();
     handleResetForm();
   }
 };
@@ -65,7 +70,7 @@ const handleSubmitForm = async () => {
       <VCard class="pa-4">
         <VCardTitle class="d-flex justify-space-between align-center">
           <div class="text-h4 font-weight-bold">Add User</div>
-          <VIcon @click="() => props.openHandler()" size="large">
+          <VIcon @click="() => props.closeHandler()" size="large">
             mdi-close-thick
           </VIcon>
         </VCardTitle>
@@ -107,6 +112,22 @@ const handleSubmitForm = async () => {
           </VRow>
           <VRow>
             <VCol cols="12">
+              <div class="font-weight-bold mb-1">Password</div>
+              <VTextField
+                v-model="formData.password"
+                :rules="formRules.password"
+                type="password"
+                placeholder="Input password"
+                density="compact"
+              >
+                <template #append>
+                  <VBtn @click="handleGeneratePassword">Generate</VBtn>
+                </template>
+              </VTextField>
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol cols="12">
               <div class="font-weight-bold mb-1">Phone Number</div>
               <VTextField
                 v-model="formData.phone_number"
@@ -144,7 +165,7 @@ const handleSubmitForm = async () => {
           <VBtn
             variant="outlined"
             class="px-4 me-3"
-            @click="() => props.openHandler()"
+            @click="() => props.closeHandler()"
             >Cancel</VBtn
           >
           <VBtn type="submit" variant="flat" class="px-7"> Add </VBtn>
