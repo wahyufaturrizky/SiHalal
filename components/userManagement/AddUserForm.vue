5c5f5ee8dbd95f03e3e1ecb867c4925e432ebe57
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2024-12-30 22:11:56 +0700

    implement design on user list page

--- components/userManagement/AddUserForm.vue
+++ components/userManagement/AddUserForm.vue
@@ -1,100 +1,155 @@
-<!-- eslint-disable @stylistic/js/spaced-comment -->
-<!-- eslint-disable lines-around-comment -->
 <script setup lang="ts">
-//data
-let formDataEmailSearch: string
-let formDataRoleId: array
-let formDataName: string
-let formDataEmail: string
-let formDataJabatan: string
-let formDataPhone: string
-let formDataNip: string
-let formDataUnit: string
+const props = defineProps<{
+  dialogVisible: boolean;
+  openHandler: Function;
+}>();
 
-const submitForm = () => {
-  console.log('submitted')
-}
+const emit = defineEmits(["update:handleSubmit"]);
+
+const form = ref();
+const formData = reactive({
+  username: null,
+  name: null,
+  email: null,
+  phone_number: null,
+  is_verified: false,
+  role: null,
+});
+const formRules = reactive({
+  username: [(v: string) => !!v || "Username is required!"],
+  name: [(v: string) => !!v || "Name is required!"],
+  email: [(v: string) => !!v || "Email is required!"],
+  phoneNumber: [(v: string) => !!v || "Phone number is required!"],
+  role: [(v: string) => !!v || "Role is required!"],
+});
+const roleDropdown = ref([
+  { title: "Pelaku Usaha", value: "Pelaku Usaha" },
+  { title: "Verifikator HLN", value: "Verifikator HLN" },
+  { title: "Keuangan", value: "Keuangan" },
+  { title: "Verifikator Fasilitator", value: "Verifikator Fasilitator" },
+  { title: "Fasilitator", value: "Fasilitator" },
+  { title: "Pendamping", value: "Pendamping" },
+  { title: "Verifikator Self-Declare", value: "Verifikator Self-Declare" },
+  { title: "Komite Fatwa", value: "Komite Fatwa" },
+  { title: "Verifikator Reguler", value: "Verifikator Reguler" },
+  { title: "Lembaga Penjamin Halal", value: "Lembaga Penjamin Halal" },
+  { title: "Auditor", value: "Auditor" },
+  { title: "Komisi Fatwa", value: "Komisi Fatwa" },
+  { title: "Admin", value: "Admin" },
+  { title: "Verifikator BPJPH", value: "Verifikator BPJPH" },
+]);
+const handleResetForm = () => {
+  Object.assign(formData, {
+    username: null,
+    name: null,
+    email: null,
+    phone_number: null,
+    is_verified: false,
+    role: null,
+  });
+};
+const handleSubmitForm = async () => {
+  const status = await form.value.validate();
+
+  if (status.valid) {
+    emit("update:handleSubmit", formData);
+    props.openHandler();
+    handleResetForm();
+  }
+};
 </script>
 
 <template>
-  <VForm @submit.prevent>
-    <VRow>
-      <VCol class="v-col-10">
-        <VTextField
-          v-model="formDataEmailSearch"
-          label="Email / NIP"
-        />
-      </VCol>
-      <VCol class="v-col-2">
-        <VBtn>Cari</VBtn>
-      </VCol>
-    </VRow>
-    <VRow>
-      <VCol class="v-col-6">
-        <VTextField
-          v-model="formDataName"
-          disabled
-          label="Nama"
-        />
-      </VCol>
-      <VCol class="v-col-6">
-        <VTextField
-          v-model="formDataEmail"
-          type="email"
-          label="Email"
-          disabled
-        />
-      </VCol>
-    </VRow>
-    <VRow>
-      <VCol class="v-col-6">
-        <VTextField
-          v-model="formDataJabatan"
-          label="Jabatan"
-          disabled
-        />
-      </VCol>
-      <VCol class="v-col-6">
-        <VTextField
-          v-model="formDataPhone"
-          label="Telepon"
-          disabled
-        />
-      </VCol>
-    </VRow>
-    <VRow>
-      <VCol class="v-col-6">
-        <VTextField
-          v-model="formDataNip"
-          label="NIP"
-          disabled
-        />
-      </VCol>
-      <VCol class="v-col-6">
-        <VTextField
-          v-model="formDataUnit"
-          label="Unit Kerja"
-          disabled
-        />
-      </VCol>
-    </VRow>
-    <VRow>
-      <VCol class="v-col-12">
-        <VSelect
-          v-model="formDataRoleId"
-          multiple
-          chips
-          label="Pilih Peran"
-        />
-      </VCol>
-    </VRow>
-    <VRow>
-      <VCol class="v-col-3">
-        <VBtn @click="submitForm">
-          Submit
-        </VBtn>
-      </VCol>
-    </VRow>
-    <br>
-  </VForm>
+  <VDialog v-model="props.dialogVisible" max-width="840px" persistent>
+    <VForm ref="form" @submit.prevent="handleSubmitForm">
+      <VCard class="pa-4">
+        <VCardTitle class="d-flex justify-space-between align-center">
+          <div class="text-h4 font-weight-bold">Add User</div>
+          <VIcon @click="() => props.openHandler()" size="large">
+            mdi-close-thick
+          </VIcon>
+        </VCardTitle>
+        <VCardItem>
+          <VRow>
+            <VCol cols="12">
+              <div class="font-weight-bold mb-1">Username</div>
+              <VTextField
+                autofocus
+                v-model="formData.username"
+                :rules="formRules.username"
+                placeholder="Input username"
+                density="compact"
+              />
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol cols="12">
+              <div class="font-weight-bold mb-1">Name</div>
+              <VTextField
+                v-model="formData.name"
+                :rules="formRules.name"
+                placeholder="Input name"
+                density="compact"
+              />
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol cols="12">
+              <div class="font-weight-bold mb-1">Email</div>
+              <VTextField
+                v-model="formData.email"
+                :rules="formRules.email"
+                type="email"
+                placeholder="Input email"
+                density="compact"
+              />
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol cols="12">
+              <div class="font-weight-bold mb-1">Phone Number</div>
+              <VTextField
+                v-model="formData.phone_number"
+                :rules="formRules.phoneNumber"
+                placeholder="Input phone number"
+                density="compact"
+              />
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol cols="12">
+              <div class="font-weight-bold">Is Verify?</div>
+              <VCheckbox
+                v-model="formData.is_verified"
+                :label="formData.is_verified ? 'Yes' : 'No'"
+              />
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol cols="12">
+              <div class="font-weight-bold mb-1">Role</div>
+              <VSelect
+                v-model="formData.role"
+                :rules="formRules.role"
+                :items="roleDropdown"
+                placeholder="Select role"
+                density="compact"
+                menu-icon="fa-chevron-down"
+                clearable
+              />
+            </VCol>
+          </VRow>
+        </VCardItem>
+        <VCardActions class="mt-7 pe-5">
+          <VBtn
+            variant="outlined"
+            class="px-4 me-3"
+            @click="() => props.openHandler()"
+            >Cancel</VBtn
+          >
+          <VBtn type="submit" variant="flat" class="px-7"> Add </VBtn>
+        </VCardActions>
+      </VCard>
+    </VForm>
+  </VDialog>
 </template>
--- pages/user-management/user.vue
+++ pages/user-management/user.vue
@@ -1,52 +1,220 @@
 <script setup lang="ts">
-const items = [
+type DataUser = {
+  id?: string;
+  username: string;
+  nama: string;
+  email: string;
+  phone_no: string;
+  is_verify: boolean;
+  role: string;
+};
+const tableHeaders: any[] = [
+  { title: "No", key: "no", sortable: false },
+  { title: "User Name", key: "username", nowrap: true },
+  { title: "Name", key: "nama", nowrap: true },
+  { title: "Email", key: "email", nowrap: true },
+  { title: "Phone Number", key: "phone_no", nowrap: true },
+  { title: "Verify", key: "is_verify", nowrap: true },
+  { title: "Role", key: "role", nowrap: true },
+  { title: "Action", key: "actions", sortable: false, align: "center" },
+];
+const tableItems = ref<Array<DataUser>>([
   {
-    'No': 1,
-    'NIP': '1232131jnjadd',
-    'Nama Jabatan': '2024-10-05',
-    'No. Handphone': '2024-12-05',
-    'Nama Lengkap': 'wdbadbaiwd',
-    'Username': 'awdbuabdiawbdi',
+    username: "Banawadmnk",
+    nama: "Banawa Damanik",
+    email: "juli18@palastri.in",
+    phone_no: "0839 9981 7991",
+    is_verify: true,
+    role: "Pelaku Usaha",
   },
-]
+  {
+    username: "Zelda_Zulaika",
+    nama: "Zelda Zulaika",
+    email: "ibrani.pradana@yahoo.co.id",
+    phone_no: "0839 9981 7991",
+    is_verify: true,
+    role: "Verifikator HLN",
+  },
+  {
+    username: "Genta_Halimah",
+    nama: "Genta Halimah",
+    email: "gmulyani@tamba.sch.id",
+    phone_no: "0759 9443 896",
+    is_verify: false,
+    role: "Fasilitator",
+  },
+]);
+const currentPage = ref(1);
+const itemPerPage = ref(10);
+const totalItems = ref(tableItems.value.length);
+const isLoading = ref(false);
+
+const searchQuery = ref("");
+const handleSearchUser = useDebounceFn((val: string) => {
+  searchQuery.value = val;
+  currentPage.value = 1;
+
+  // refresh();
+}, 350);
+
+const isOpenAddModal = ref(false);
+const handleOpenAddModal = () => {
+  isOpenAddModal.value = !isOpenAddModal.value;
+};
+const handleAddNewUser = (payload: any) => {
+  console.log(payload, "< submit payload");
+  useSnackbar().sendSnackbar("Data Successfully Added", "success");
+  // useSnackbar().sendSnackbar("Add Data Failed", "error");
+};
+
+const selectedUser = ref("");
+const isOpenDeleteModal = ref(false);
+const handleOpenDeleteModal = (id?: string | null) => {
+  if (id) selectedUser.value = id;
+  isOpenDeleteModal.value = !isOpenDeleteModal.value;
+};
+
+const handleConfirmDelete = () => {
+  useSnackbar().sendSnackbar("Data Successfully Deleted", "success");
+  // useSnackbar().sendSnackbar("Delete Data Failed", "error");
+};
 </script>
 
 <template>
-  <div>
-    <VRow>
-      <VCol>
-        <VCard class="w-100">
-          <VCardTitle>User Management</VCardTitle>
-          <VCardItem>
-            <VRow>
-              <VCol class="v-align-end">
-                <HalalModal
-                  title="Add User"
-                  parent-btn-label="Add User"
-                  :card-width="700"
-                >
-                  <template #content>
-                    <AddUserForm />
+  <VRow>
+    <VCol>
+      <h1 class="mb-3 font-weight-bold">User</h1>
+    </VCol>
+  </VRow>
+  <VRow>
+    <VCol>
+      <VCard class="w-100 py-3">
+        <VCardTitle
+          class="d-flex justify-space-between align-center font-weight-bold text-h4"
+        >
+          <div>User List</div>
+          <VBtn append-icon="fa-plus" @click="handleOpenAddModal"
+            >Add User</VBtn
+          >
+        </VCardTitle>
+        <VCardItem>
+          <VRow>
+            <VCol cols="6">
+              <VTextField
+                placeholder="Search Data"
+                v-model="searchQuery"
+                @update:model-value="handleSearchUser"
+                clearable
+                density="compact"
+                class="mb-5"
+                append-inner-icon="fa-search"
+              />
+            </VCol>
+          </VRow>
+          <VCard variant="outlined">
+            <VDataTableServer
+              class="custom-table"
+              :headers="tableHeaders"
+              :items="tableItems"
+              :items-length="totalItems"
+              v-model:items-per-page="itemPerPage"
+              v-model:page="currentPage"
+              :loading="isLoading"
+              :hide-default-footer="tableItems.length === 0"
+            >
+              <template #no-data>
+                <VCard variant="outlined" class="w-full mt-7 mb-5">
+                  <div class="pt-2" style="justify-items: center">
+                    <img
+                      src="~/assets/images/empty-data.png"
+                      alt="empty_data"
+                    />
+                    <div class="pt-2 pb-2 font-weight-bold">Data Kosong</div>
+                  </div>
+                </VCard>
+              </template>
+              <template #item.no="{ index }">
+                {{ index + 1 + (currentPage - 1) * itemPerPage }}
+              </template>
+              <template #item.is_verify="{ item }">
+                {{ item.is_verify ? "Yes" : "No" }}
+              </template>
+              <template #item.actions="{ item }">
+                <VMenu>
+                  <template #activator="{ props }">
+                    <VIcon
+                      v-bind="props"
+                      icon="fa-ellipsis-v"
+                      color="primary"
+                    />
                   </template>
-                </HalalModal>
-              </VCol>
-            </VRow>
-            <VRow>
-              <VCol>
-                <VTextField
-                  label="Searching"
-                  clearable
-                />
-              </VCol>
-            </VRow>
-            <VRow>
-              <VCol>
-                <VDataTable :items="items" />
-              </VCol>
-            </VRow>
-          </VCardItem>
-        </VCard>
-      </VCol>
-    </VRow>
-  </div>
+                  <VList>
+                    <VListItem class="cursor-pointer">
+                      <template #prepend>
+                        <VIcon icon="fa-rotate-right" :size="16" />
+                      </template>
+                      <VListItemTitle>Reset Password</VListItemTitle>
+                    </VListItem>
+                    <VListItem
+                      class="cursor-pointer"
+                      @click="handleOpenDeleteModal(item.id)"
+                    >
+                      <template #prepend>
+                        <VIcon icon="mdi-delete" color="error" />
+                      </template>
+                      <VListItemTitle class="text-error">Hapus</VListItemTitle>
+                    </VListItem>
+                  </VList>
+                </VMenu>
+              </template>
+            </VDataTableServer>
+          </VCard>
+        </VCardItem>
+      </VCard>
+    </VCol>
+  </VRow>
+  <AddUserForm
+    :dialog-visible="isOpenAddModal"
+    :open-handler="handleOpenAddModal"
+    @update:handle-submit="handleAddNewUser"
+  >
+  </AddUserForm>
+  <ConfirmModal
+    dialog-title="Delete Confirmation"
+    :dialog-visible="isOpenDeleteModal"
+    @update:dialog-visible="isOpenDeleteModal = $event"
+  >
+    <VCardText> Are you sure you want to delete this data? </VCardText>
+    <VCardActions class="px-4">
+      <VBtn variant="outlined" class="px-4 me-3" @click="handleOpenDeleteModal"
+        >Cancel</VBtn
+      >
+      <VBtn
+        variant="flat"
+        class="px-4"
+        color="error"
+        @click="[handleConfirmDelete(), handleOpenDeleteModal()]"
+      >
+        Delete
+      </VBtn>
+    </VCardActions>
+  </ConfirmModal>
 </template>
+
+<style scoped lang="scss">
+:deep(.v-data-table.custom-table > .v-table__wrapper) {
+  table {
+    thead > tr > th:last-of-type {
+      right: 0;
+      position: sticky;
+      border-left: 1px solid rgba(#000000, 0.12);
+    }
+    tbody > tr > td:last-of-type {
+      right: 0;
+      position: sticky;
+      border-left: 1px solid rgba(#000000, 0.12);
+      background: white;
+    }
+  }
+}
+</style>
