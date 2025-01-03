5aa7e017e3e6525f2fdd1534c38af4b7841a6448
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2025-01-03 22:34:40 +0700

    Merge branch 'development' of https://gitlab.com/geryn25/kemenang-sihalal-frontend into integration/administrator-pages

--- pages/user-management/user.vue
+++ pages/user-management/user.vue
@@ -268,15 +268,16 @@ const handleConfirmDelete = async () => {
 :deep(.v-data-table.custom-table > .v-table__wrapper) {
   table {
     thead > tr > th:last-of-type {
-      right: 0;
       position: sticky;
-      border-left: 1px solid rgba(#000000, 0.12);
+      border-inline-start: 1px solid rgba(#000, 0.12);
+      inset-inline-end: 0;
     }
+
     tbody > tr > td:last-of-type {
-      right: 0;
       position: sticky;
-      border-left: 1px solid rgba(#000000, 0.12);
       background: white;
+      border-inline-start: 1px solid rgba(#000, 0.12);
+      inset-inline-end: 0;
     }
   }
 }
