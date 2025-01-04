c95cbc4f70689d1221407ccb3d548892401a7ec9
Andria Bakti Mahendra <andriabaktimahendra@gmail.com>
2025-01-04 23:56:23 +0700

    Adjust API handler for load random image

--- server/api/admin/images/random-image.get.ts
+++ server/api/admin/images/random-image.get.ts
@@ -1,24 +1,12 @@
 import type { NuxtError } from "nuxt/app";
 
 export default defineEventHandler(async (event: any) => {
-  const authHeader = getRequestHeader(event, "Authorization");
-  if (typeof authHeader === "undefined") {
-    throw createError({
-      statusCode: 403,
-      statusMessage:
-        "Need to pass valid Bearer-authorization header to access this endpoint",
-    });
-  }
-
   try {
     const runtimeConfig = useRuntimeConfig();
-    const query: any = await getQuery(event);
-
     const response = await $fetch(
-      `${runtimeConfig.authBaseUrl}/api/v1/admin/image-auth/random`,
+      `${runtimeConfig.authBaseUrl}/api/v1/image-auth/random`,
       {
         method: "get",
-        headers: { Authorization: authHeader },
       } as any
     );
 
