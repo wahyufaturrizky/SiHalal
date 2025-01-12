60289fbc1d2e9755c85eef3e6c3b00a6f55db6f5
arvin018 <arvin.aditama@gmail.com>
2025-01-12 15:22:00 +0700

    update-vertical-menu

--- stores/authUser.ts
+++ stores/authUser.ts
@@ -7,56 +7,18 @@ export const useMyAuthUserStore = defineStore("myAuthUserStore", () => {
   async function getSession() {
     if (user.value == null) {
       user.value = await auth.getSession();
-      if (user.value && process.client) {
-        localStorage.setItem("authUser", JSON.stringify(user.value));
-      }
     }
     return user;
   }
 
   function resetUser() {
     user.value = null;
-    // if (process.client) {
-    //   // Hapus dari localStorage jika di sisi klien
-    //   localStorage.removeItem("authUser");
-    // }
   }
-
-  function loadUserFromLocalStorage() {
-    if (process.client) {
-      // Pastikan hanya berjalan di browser
-      const storedData = localStorage.getItem("authUser");
-      if (storedData) {
-        try {
-          user.value = JSON.parse(storedData);
-        } catch (error) {
-          console.error("Error parsing JSON from localStorage", error);
-        }
-      }
-    }
-  }
-  loadUserFromLocalStorage();
 
   function canAccess(roles: string | string[]) {
     const roleArray = Array.isArray(roles) ? roles : [roles];
     return user.value?.roles.some((data) => roleArray.includes(data.name));
   }
 
-  // id: "myAuthUserStore",
-  // state: () => ({
-  //   user: null as void | SessionData | null,
-  //   auth: useAuth(),
-  // }),
-  // actions: {
-  //   async getSession() {
-  //     if (this.user == null) {
-  //       this.setUser(await this.auth.getSession());
-  //     }
-  //     return this.user;
-  //   },
-  //   async setUser(data: any) {
-  //     this.user = data;
-  //   },
-  // },
   return { user, getSession, resetUser, canAccess };
 });
