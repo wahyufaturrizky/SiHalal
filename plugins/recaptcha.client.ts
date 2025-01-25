ba669b5ce51687e58feb07b5e30ef9b5fdeed5e6
geryn25 <35487884+geryn25@users.noreply.github.com>
2025-01-25 14:02:22 +0700

    Update recaptcha.client.ts

--- plugins/recaptcha.client.ts
+++ plugins/recaptcha.client.ts
@@ -1,6 +1,7 @@
 import { install } from "vue3-recaptcha-v2";
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
+  console.log(config.public.recaptcha.siteKey);
   nuxtApp.vueApp.use(install, {
     sitekey: config.public.recaptcha.siteKey,
     cndomain: false,
