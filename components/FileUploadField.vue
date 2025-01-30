dc66103e81b5841c24d295cf71b8521f8b0069b6
Farezas <fareza087@gmail.com>
2025-01-30 16:53:41 +0700

    fix: komisi fatwa profile remove required file, refresh kabupaten and kecamatan

--- components/FileUploadField.vue
+++ components/FileUploadField.vue
@@ -6,30 +6,49 @@ const props = defineProps<{
   fileData: File | null;
   fileName: string | null;
   validationList?: Array<any> | null;
+  initialFileName: String | null;
 }>();
 
 const fileInputRef = ref<VFileInput>();
 
 const fileValidationMessage = ref();
 const fileIsValid = ref(false);
+const evaluatedFile = ref(false);
+
+const updatedFileData = computed(() => props.fileData);
+const updatedFileName = computed(() => props.fileName);
+const initialFileName = computed(() => props.initialFileName);
+
+const fileValidationRulesList = ref([]);
 
 const validateFile = async () => {
   if (!fileInputRef.value) {
-    console.warn("fileInputRef is not defined");
+    // console.warn("fileInputRef is not defined");
     return;
   }
 
   try {
     const result = await fileInputRef.value.validate();
-    // console.log("Validation result:", result[0]);
+    console.log("Validation result:", result[0]);
+    console.log("initial name = ", props.initialFileName);
 
     if (result?.length === parseInt("0")) {
-      console.log("emited true");
+      // console.log("emited true");
       fileIsValid.value = true;
+      evaluatedFile.value = false;
+
       emits("validationIsValid", true);
     } else {
+      if (initialFileName.value === "") {
+        console.log("filename false");
+        evaluatedFile.value = true;
+      } else {
+        console.log("filename true");
+        evaluatedFile.value = false;
+      }
       fileIsValid.value = false;
       fileValidationMessage.value = result[0];
+
       emits("validationIsValid", false, result[0]);
     }
   } catch (error) {
@@ -38,15 +57,14 @@ const validateFile = async () => {
   }
 };
 
-const updatedFileData = computed(() => props.fileData);
-const updatedFileName = computed(() => props.fileName);
-
-const fileValidationRulesList = ref([requiredValidator]);
+const prependFileName = ref();
 
 watch(
   () => props.validationList,
   (newData) => {
-    fileValidationRulesList.value = [requiredValidator];
+    if (updatedFileName.value === "") {
+      fileValidationRulesList.value = [requiredValidator];
+    }
     if (newData) {
       fileValidationRulesList.value = [
         ...fileValidationRulesList.value,
@@ -58,20 +76,33 @@ watch(
   { immediate: true }
 );
 
+const setPrependFileName = (value: string | null) => {
+  prependFileName.value = value;
+};
+
 watch(
-  () => props.fileData || props.fileName,
-  async (newData) => {
+  () => props.fileName,
+  async (newData1, newData2) => {
+    prependFileName.value = newData1;
     await validateFile();
   },
   { immediate: true }
 );
 
-defineExpose({ validateFile });
+defineExpose({ validateFile, setPrependFileName });
 </script>
 
 <template>
-  <div v-if="updatedFileData || updatedFileName">
-    <VTextField
+  <!-- <div v-if="updatedFileData || updatedFileName"> -->
+  <!-- <VTextField
+      :rules="[
+        requiredValidator,
+        regexValidator(
+          updatedFileName,
+          /^[a-zA-Z0-9\s\(\)-_]+(.jpg|.png|.pdf|.jpeg)$/,
+          'File extension not allowed'
+        ),
+      ]"
       :model-value="updatedFileName"
       density="compact"
       placeholder="No file choosen"
@@ -85,8 +116,8 @@ defineExpose({ validateFile });
           @click="() => emits('onRemove')"
         />
       </template>
-    </VTextField>
-    <div v-if="!fileIsValid" class="v-input__details">
+    </VTextField> -->
+  <!-- <div class="v-input__details" v-if="evaluatedFile">
       <div
         class="v-messages"
         role="alert"
@@ -94,25 +125,38 @@ defineExpose({ validateFile });
         id="input-698-messages"
         style="color: rgb(var(--v-theme-error)); padding-inline: 16px"
       >
-        {{ fileValidationMessage }}
+        {{ fileValidationMessage }} 
+        {{ initialFileName }}
       </div>
-    </div>
-  </div>
+    </div> -->
+  <!-- </div> -->
   <VFileInput
-    v-else
     ref="fileInputRef"
     :model-value="updatedFileData"
     :rules="fileValidationRulesList"
     class="custom-file-input"
     density="compact"
     rounded="xl"
-    label="No file choosen"
+    :label="prependFileName ? '' : 'No file choosen'"
     prepend-icon=""
     @change="(e: any) => emits('onSelect', e)"
     accept="image/*"
   >
     <template #append-inner>
       <VBtn rounded="s-0 e-xl" text="Choose File" />
+    </template>
+    <template #prepend-inner>
+      <span
+        style="white-space: nowrap"
+        v-if="initialFileName && prependFileName"
+      >
+        {{ prependFileName?.substring(0, 20) + "..." }}
+      </span>
+      <!-- <VTextField
+        style="white-space: nowrap; text-overflow: ellipsis"
+        v-if="initialFileName"
+        variant="plain"
+      ></VTextField> -->
     </template>
   </VFileInput>
 </template>
