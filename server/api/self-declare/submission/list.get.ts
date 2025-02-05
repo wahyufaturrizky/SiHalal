d589339668d1d5fd69121226f818db26b212ae6d
wahyu.rizki <wahyu.rizki@accenture.com>
2025-02-05 21:24:33 +0700

    feat: Add channel_id parameter to self-declare submission queries

--- server/api/self-declare/submission/list.get.ts
+++ server/api/self-declare/submission/list.get.ts
@@ -12,17 +12,33 @@ export default defineEventHandler(async (event: any) => {
 
   try {
     const runtimeConfig = useRuntimeConfig();
-    const query: any = await getQuery(event);
-    const params = {
-      page: isNaN(Number.parseInt(query.page, 10))
-        ? 1
-        : Number.parseInt(query.page, 10),
-      size: isNaN(Number.parseInt(query.size, 10))
-        ? 1
-        : Number.parseInt(query.size, 10),
-      keyword: query.keyword,
-      status: query.status,
+
+    const { page, size, keyword, status, channel_id } = (await getQuery(
+      event
+    )) as {
+      page: string;
+      size: string;
+      keyword: string;
+      status: string;
+      channel_id: string;
     };
+
+    const params: any = {
+      page: isNaN(Number.parseInt(page, 10)) ? 1 : Number.parseInt(page, 10),
+      size: isNaN(Number.parseInt(size, 10)) ? 10 : Number.parseInt(size, 10),
+    };
+
+    if (keyword !== "") {
+      params["keyword"] = keyword;
+    }
+
+    if (channel_id !== "") {
+      params["channel_id"] = channel_id;
+    }
+
+    if (status !== "") {
+      params["status"] = status;
+    }
 
     const response = await $fetch(
       `${runtimeConfig.coreBaseUrl}/api/v1/halal-certificate-reguler/submission/self-declare`,
