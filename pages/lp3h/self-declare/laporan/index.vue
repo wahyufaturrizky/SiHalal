084f890b33a1f417bd63e999920585251df77cb5
amevide998 <hdsvidi@gmail.com>
2025-02-12 23:58:49 +0700

    CREATE PAGE FORCE TASK 3-1 , 3-5, 3-6, 3-7

---
+++ pages/lp3h/self-declare/laporan/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <VContainer>
+    <VRow>
+      <KembaliButton />
+    </VRow>
+    <VRow class="d-flex justify-space-between align-center">
+      <VCol class="">
+        <h3 class="text-h3">
+          Laporan Self Declare
+        </h3>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
+
+<style scoped lang="scss">
+
+</style>
