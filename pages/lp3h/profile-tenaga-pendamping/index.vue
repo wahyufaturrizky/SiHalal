f78b6f6f50ff28cfcaf19f9cdba66e3cc1330d67
arvin018 <arvin.aditama@gmail.com>
2025-02-19 09:47:47 +0700

    update

--- pages/lp3h/profile-tenaga-pendamping/index.vue
+++ pages/lp3h/profile-tenaga-pendamping/index.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
+const panelProfile = ref([0, 1]);
+const panelBank = ref([0, 1]);
+const panelPenanggungJawab = ref([0, 1]);
 
-const panelProfile = ref([0, 1])
-const panelPenanggungJawab = ref([0, 1])
-
-const panelDataRegistrasi = ref([0,1])
-const panelDokumenPersyaratan = ref([0,1])
-const panelMelacak = ref([0,1])
-
+const panelDataRegistrasi = ref([0, 1]);
+const panelDokumenPersyaratan = ref([0, 1]);
+const panelMelacak = ref([0, 1]);
 
 const dataProfilePendamping = [
   { label: "NIK", value: "322323232332" },
   { label: "Nama", value: "John Fren" },
-  { label: "Alamat", value: "Sumbawa Banget, RT002/RW002, Sumbang, Curio Sumbawa Banget, RT002/RW002, Sumbang, Curio" },
+  {
+    label: "Alamat",
+    value:
+      "Sumbawa Banget, RT002/RW002, Sumbang, Curio Sumbawa Banget, RT002/RW002, Sumbang, Curio",
+  },
   { label: "Provinsi", value: "Jawa Tengah" },
   { label: "Kota/Kab", value: "Kota Semarang" },
   { label: "Kecamatan", value: "Ngaliyan" },
@@ -20,68 +23,70 @@ const dataProfilePendamping = [
   { label: "Telp /HP", value: "0812345678978" },
   { label: "Tempat Lahir", value: "Surabaya" },
   { label: "Pekerjaaan", value: "Lainnya" },
+];
 
+const dataBank = [
+  { label: "Nama Bank", value: "Bank Syariah Indonesia" },
+  { label: "No. Rekening", value: "0821178958123" },
+  { label: "Nama Rekening", value: "M Rizqi Maulana" },
+  { label: "File Rekeneing", value: "-" },
+];
 
-]
+const dataBank2 = [
+  { label: "NPWP", value: "123.456.789.761" },
+  { label: "Nama pada NPWP", value: "M maulana" },
+];
 
 const dataNamaPenanggungJawab = [
   { label: "Nama Pimpinan", value: "Albert Flores" },
-  { label: "No. HP Pimpinan", value: "0821178958123" }
-]
+  { label: "No. HP Pimpinan", value: "0821178958123" },
+];
 
 const dataKontakPenanggungJawab = [
   { label: "Nama Kontak", value: "Robert Fox" },
-  { label: "No. HP Kontak", value: "0821178958123" }
-]
-
-
-
+  { label: "No. HP Kontak", value: "0821178958123" },
+];
 
 const deleteItem = (item) => {
-  console.log("ITEM DELETE : ", item)
-}
+  console.log("ITEM DELETE : ", item);
+};
 
 const previewIjazah = (item) => {
-  console.log("PREVIEW IJAZAH : ", item)
-}
+  console.log("PREVIEW IJAZAH : ", item);
+};
 
 const previewKtp = (item) => {
-  console.log("PREVIEW KTP : ", item)
-}
+  console.log("PREVIEW KTP : ", item);
+};
 
 const dataRegistrasi = [
   { label: "Status", value: "Disetujui" },
   { label: "No. Registrasi", value: "1234567890789" },
-  { label: "Tanggal Berlaku", value: "-" }
-]
+  { label: "Tanggal Berlaku", value: "-" },
+];
 
-const documentLMS =[
-  { name: "Pendalaman LMS" , id: "1"},
-]
+const documentLMS = [{ name: "Pendalaman LMS", id: "1" }];
 
 const dokumenPersyaratan = [
-  { name: "Ijazah" , id: "1"},
+  { name: "Ijazah", id: "1" },
   { name: "KTP", id: "2" },
-  { name: "Sertifikat Pelatihan", id: "3" }
-
-]
+  { name: "Sertifikat Pelatihan", id: "3" },
+];
 
 const deleteDokumenPersyaratan = (item) => {
-  console.log("DELETE DOKUMEN PERSYARATAN : ", item)
-}
+  console.log("DELETE DOKUMEN PERSYARATAN : ", item);
+};
 
 const downloadDokumenPersyaratan = (item) => {
-  console.log("Download Dokumen Peryaratan : ", item)
-}
-
+  console.log("Download Dokumen Peryaratan : ", item);
+};
 
 const dialog = ref(false);
 const ajukanHandler = () => {
-  console.log("AJUKAN SEBAGAI LEMBAGA PENDAMPING : ")
-  dialog.value = false
-  useSnackbar().sendSnackbar("Berhasil mengirim pengajuan ", "success")
-}
-
+  console.log("AJUKAN SEBAGAI LEMBAGA PENDAMPING : ");
+  dialog.value = false;
+  useSnackbar().sendSnackbar("Berhasil mengirim pengajuan ", "success");
+};
 </script>
 
 <template>
@@ -103,9 +108,7 @@ const ajukanHandler = () => {
     </VRow> -->
     <VRow class="d-flex justify-space-between align-center">
       <VCol class="mb-8">
-        <h3 class="text-h3">
-          Tenaga Pendamping
-        </h3>
+        <h3 class="text-h3">Tenaga Pendamping</h3>
       </VCol>
       <!-- <VCol cols="8">
         <VRow class="d-flex justify-end align-center ga-2">
@@ -133,30 +136,28 @@ const ajukanHandler = () => {
             <VExpansionPanelText>
               <VRow v-for="(item, index) in dataProfilePendamping" :key="index">
                 <VCol cols="3" class="text-left font-weight-medium">
-                  {{ item.label }}  
+                  {{ item.label }}
                 </VCol>
-                <VCol cols="1" class="text-left">
-                  :
-                </VCol>
+                <VCol cols="1" class="text-left"> : </VCol>
                 <VCol cols="12" class="pl-1 font-weight-medium">
                   <VTextField
-                   v-if="item.label !== 'Alamat'"
-                   v-model="item.value"
+                    v-if="item.label !== 'Alamat'"
+                    v-model="item.value"
                     variant="outlined"
-                   density="compact"
-                   hide-details
-                  readonly
+                    density="compact"
+                    hide-details
+                    readonly
                   />
-             <VTextarea
-              v-else
-              v-model="item.value"
-              variant="outlined"
-              density="compact"
-              hide-details
-              readonly
-              rows="2"
-              auto-grow
-               />
+                  <VTextarea
+                    v-else
+                    v-model="item.value"
+                    variant="outlined"
+                    density="compact"
+                    hide-details
+                    readonly
+                    rows="2"
+                    auto-grow
+                  />
                 </VCol>
               </VRow>
             </VExpansionPanelText>
@@ -168,18 +169,18 @@ const ajukanHandler = () => {
               Penanggung Jawab
             </VExpansionPanelTitle>
             <VExpansionPanelText>
-              <VRow v-for="(item, index) in dataNamaPenanggungJawab" :key="index">
+              <VRow
+                v-for="(item, index) in dataNamaPenanggungJawab"
+                :key="index"
+              >
                 <VCol cols="5" class="text-left font-weight-medium">
                   {{ item.label }}
                 </VCol>
-                <VCol cols="1" class="text-right">
-                  :
-                </VCol>
+                <VCol cols="1" class="text-right"> : </VCol>
                 <VCol cols="6" class="pl-0">
                   {{ item.value }}
                 </VCol>
               </VRow>
-           
             </VExpansionPanelText>
           </VExpansionPanel>
         </VExpansionPanels>
@@ -189,65 +190,119 @@ const ajukanHandler = () => {
               Melacak
             </VExpansionPanelTitle>
             <VExpansionPanelText>
-              <HalalTimeLine :event="[
-                { created_at: '09/09/2024',
-                  id: '1',
-                  status: 'Pengajuan',
-                  username: 'Samsul',
-                  comment: ''},
-              ]" />
+              <HalalTimeLine
+                :event="[
+                  {
+                    created_at: '09/09/2024',
+                    id: '1',
+                    status: 'Pengajuan',
+                    username: 'Samsul',
+                    comment: '',
+                  },
+                ]"
+              />
             </VExpansionPanelText>
           </VExpansionPanel>
         </VExpansionPanels>
       </VCol>
       <VCol cols="4">
-
         <VCard class="mb-4">
-  <VCardTitle class="text-h4 font-weight-bold">
-    LMS GreatEdu
-  </VCardTitle>
-  <VCardText>
-    <VList class="mb-4">
-      <VListItem v-for="(item, index) in documentLMS" :key="index" class="pa-1">
-        <VRow class="d-flex align-center">
-          <VCol cols="12" md="8">
-            <div class="text-body-1 font-weight-medium">{{ item.name }}</div>
-          </VCol>
-          <VCol cols="12" md="4" class="squareBtnIcon">
-                      :<VBtn icon variant="outlined" color="purple" class="ml-2" @click="downloadDokumenPersyaratan(item)">
-                        <VIcon color="primary">mdi-download</VIcon>
-                      </VBtn>
-                    </VCol>
-        </VRow>
-      </VListItem>
-    </VList>
-  </VCardText>
-</VCard>
-
+          <VCardTitle class="text-h4 font-weight-bold">
+            LMS GreatEdu
+          </VCardTitle>
+          <VCardText>
+            <VList class="mb-4">
+              <VListItem
+                v-for="(item, index) in documentLMS"
+                :key="index"
+                class="pa-1"
+              >
+                <VRow class="d-flex align-center">
+                  <VCol cols="12" md="8">
+                    <div class="text-body-1 font-weight-medium">
+                      {{ item.name }}
+                    </div>
+                  </VCol>
+                  <VCol cols="12" md="4" class="squareBtnIcon">
+                    :<VBtn
+                      icon
+                      variant="outlined"
+                      color="purple"
+                      class="ml-2"
+                      @click="downloadDokumenPersyaratan(item)"
+                    >
+                      <VIcon color="primary">mdi-download</VIcon>
+                    </VBtn>
+                  </VCol>
+                </VRow>
+              </VListItem>
+            </VList>
+          </VCardText>
+        </VCard>
 
         <VExpansionPanels v-model="panelDataRegistrasi" class="mb-4">
           <VExpansionPanel>
             <VExpansionPanelTitle class="text-h4 font-weight-bold">
-            Status Registrasi
+              Status Registrasi
             </VExpansionPanelTitle>
             <VExpansionPanelText>
-              <VRow v-for="(item, index) in dataRegistrasi" :key="index" class="d-flex justify-center align-center">
+              <VRow
+                v-for="(item, index) in dataRegistrasi"
+                :key="index"
+                class="d-flex justify-center align-center"
+              >
                 <VCol cols="5" class="text-left font-weight-medium">
                   {{ item.label }}
                 </VCol>
-                <VCol cols="1" class="text-right">
-                  :
-                </VCol>
-                <VCol cols="6" class="pl-0 ">
-                  <VChip v-if="item.label === 'Status'" :color="item.value === 'Disetujui' ? 'success' : 'error'"
-                         variant="outlined"
-                         label
+                <VCol cols="1" class="text-right"> : </VCol>
+                <VCol cols="6" class="pl-0">
+                  <VChip
+                    v-if="item.label === 'Status'"
+                    :color="item.value === 'Disetujui' ? 'success' : 'error'"
+                    variant="outlined"
+                    label
                   >
                     {{ item.value }}
                   </VChip>
                   <span v-else>
                     {{ item.value }}
                   </span>
+                </VCol>
+              </VRow>
+            </VExpansionPanelText>
+          </VExpansionPanel>
+        </VExpansionPanels>
+
+        <VExpansionPanels v-model="panelBank" class="mb-4">
+          <VExpansionPanel>
+            <VExpansionPanelTitle class="text-h4 font-weight-bold">
+              Data Bank
+            </VExpansionPanelTitle>
+            <VExpansionPanelText>
+              <VRow v-for="(item, index) in dataBank" :key="index">
+                <VCol cols="3" class="text-left font-weight-medium">
+                  {{ item.label }}
+                </VCol>
+                <VCol cols="1" class="text-left"> : </VCol>
+                <VCol cols="12" class="pl-1 font-weight-medium">
+                  <VTextField
+                    v-if="item.label !== 'Alamat'"
+                    v-model="item.value"
+                    variant="outlined"
+                    density="compact"
+                    hide-details
+                    readonly
+                  />
+                  <VTextarea
+                    v-else
+                    v-model="item.value"
+                    variant="outlined"
+                    density="compact"
+                    hide-details
+                    readonly
+                    rows="2"
+                    auto-grow
+                  />
                 </VCol>
               </VRow>
             </VExpansionPanelText>
@@ -261,13 +316,25 @@ const ajukanHandler = () => {
             </VExpansionPanelTitle>
             <VExpansionPanelText>
               <VList class="mb-4">
-                <VListItem v-for="(item, index) in dokumenPersyaratan" :key="index" class="pa-1">
+                <VListItem
+                  v-for="(item, index) in dokumenPersyaratan"
+                  :key="index"
+                  class="pa-1"
+                >
                   <VRow class="d-flex align-center">
                     <VCol cols="12" md="8">
-                      <div class="text-body-1 font-weight-medium ">{{ item.name }}</div>
+                      <div class="text-body-1 font-weight-medium">
+                        {{ item.name }}
+                      </div>
                     </VCol>
-                  <VCol cols="12" md="4" class="squareBtnIcon">
-                      :<VBtn icon variant="outlined" color="purple" class="ml-2" @click="downloadDokumenPersyaratan(item)">
+                    <VCol cols="12" md="4" class="squareBtnIcon">
+                      :<VBtn
+                        icon
+                        variant="outlined"
+                        color="purple"
+                        class="ml-2"
+                        @click="downloadDokumenPersyaratan(item)"
+                      >
                         <VIcon color="primary">mdi-download</VIcon>
                       </VBtn>
                     </VCol>
@@ -293,8 +360,6 @@ const ajukanHandler = () => {
             </VExpansionPanelText>
           </VExpansionPanel>
         </VExpansionPanels>
-
-
       </VCol>
     </VRow>
   </VContainer>
