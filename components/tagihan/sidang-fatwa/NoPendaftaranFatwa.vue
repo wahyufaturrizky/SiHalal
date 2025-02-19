2c30fe3ebeadfbd423303c1985392dba1c444f28
adek ferdian <adekferdian@gmail.com>
2025-02-19 21:37:44 +0700

    feat integrasi: 9-2 Komisi Fatwa - Buat Tagihan Ke BPJPH feat slicing: 9-1 Komisi Fatwa - Proses Sidang

---
+++ components/tagihan/sidang-fatwa/NoPendaftaranFatwa.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+})
+const paneSwitcher = ref([0, 1])
+</script>
+
+<template>
+  <VRow
+    ><VCol cols="12">
+      <VExpansionPanels v-model="paneSwitcher">
+        <VExpansionPanel>
+          <VExpansionPanelTitle><h3>
+            {{ t('task-force.proses-sidang.detail.section-regis-no') }}
+          </h3></VExpansionPanelTitle>
+          <VExpansionPanelText>
+            <div>SH2023-1-582897</div>
+            <div>13/10/2024</div>
+            <div>Jawa Tengah</div>
+          </VExpansionPanelText>
+        </VExpansionPanel>
+      </VExpansionPanels> </VCol
+  ></VRow>
+</template>
---
+++ server/api/bill/bpjph/list.get.ts
@@ -0,0 +1,42 @@
+import { NuxtError } from 'nuxt/app';
+
+export default defineEventHandler(async (event: any) => {
+  const authHeader = getRequestHeader(event, 'Authorization');
+  if (typeof authHeader === 'undefined') {
+    throw createError({
+      statusCode: 403,
+      statusMessage:
+        'Need to pass valid Bearer-authorization header to access this endpoint',
+    });
+  }
+
+  try {
+    const runtimeConfig = useRuntimeConfig();
+
+    const { page, size } = (await getQuery(event)) as {
+      page: string
+      size: string
+    }
+
+    const params = {
+      page: +page,
+      size: +size,
+    }
+
+    const response = await $fetch(
+      `${runtimeConfig.coreBaseUrl}/api/v1/komisi-fatwa-invoice`,
+      {
+        method: 'get',
+        headers: { Authorization: authHeader },
+        params,
+      } as any,
+    )
+
+    return response || null
+  }
+  catch (error) {
+    setResponseStatus(event, 400)
+
+    return (error as NuxtError).data
+  }
+});
