3acb4fa2889f76a91ee84697037f3a677c5b08fa
Arvin Aditama Santoso <arvin.santoso_int@allobank.com>
2025-02-25 18:19:38 +0700

    udpate string tidak kosong atau null,

--- pages/lp3h/information-sertifikasi-pendamping/index.vue
+++ pages/lp3h/information-sertifikasi-pendamping/index.vue
@@ -195,10 +195,8 @@ const handleInput = (e: any) => {
 
 
 function formatDate(isoString: string): string {
-  if(isoString!=null){
-
+  if(isoString!=null||isoString!=""){
   const date = new Date(isoString);
-
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-based
   const year = date.getFullYear();
