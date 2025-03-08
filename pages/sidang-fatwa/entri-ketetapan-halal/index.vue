665b33d0f0c2c4b0d4400e3f2acba39444ad067d
adek ferdian <adekferdian@gmail.com>
2025-03-08 14:24:17 +0700

    fixing reguler

--- pages/sidang-fatwa/entri-ketetapan-halal/index.vue
+++ pages/sidang-fatwa/entri-ketetapan-halal/index.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
 import { onMounted, ref } from "vue";
 import { VDataTableServer } from "vuetify/components";
+const { t } = useI18n();
 
 const loadingAll = ref(true);
+const sessionData = await useMyAuthUserStore().getSession();
+const isKomisiPusat = sessionData?.value?.name === 'Komisi Fatwa Pusat'
 
 const items = ref([]);
 const itemPerPage = ref(10);
 const totalItems = ref(0);
 const loading = ref(false);
+const showFilterMenu = ref(false);
 const page = ref(1);
 const searchQuery = ref("");
+const filterProvinsi = ref([]);
+const selectedProvince = ref("")
 
 const loadItem = async ({
   page,
   size,
   keyword,
+  provinsi,
 }: {
   page: number;
   size: number;
   keyword: string;
+  provinsi: string;
 }) => {
   try {
     loading.value = true;
@@ -29,6 +38,7 @@ const loadItem = async ({
         page,
         size,
         keyword,
+        provinsi,
       },
     });
 
@@ -54,7 +64,37 @@ const handleInput = () => {
     page: page.value,
     size: itemPerPage.value,
     keyword: searchQuery.value,
+    provinsi: selectedProvince.value,
   });
+};
+
+const getProvince = async () => {
+  const response: any = await $api(
+    "/sidang-fatwa/task-force/provinsi",
+    {
+      method: "get",
+    }
+  );
+  filterProvinsi.value = response.data
+}
+
+const applyFilters = () => {
+  loadItem({
+    page: page.value,
+    size: itemPerPage.value,
+    keyword: searchQuery.value,
+    provinsi: selectedProvince.value,
+  })
+};
+
+const reset = () => {
+  selectedProvince.value = ''
+  loadItem({
+    page: page.value,
+    size: itemPerPage.value,
+    keyword: searchQuery.value,
+    provinsi: selectedProvince.value,
+  })
 };
 
 onMounted(async () => {
@@ -63,7 +103,9 @@ onMounted(async () => {
       page: page.value,
       size: itemPerPage.value,
       keyword: searchQuery.value,
+      provinsi: selectedProvince.value,
     }),
+    getProvince(),
   ]);
 
   const checkResIfUndefined = res.every((item: any) => {
@@ -105,6 +147,53 @@ const navigateAction = (id: string) => {
           <div class="text-h4 font-weight-bold">Data Pengajuan</div>
         </VCol>
       </VRow>
+      <VRow v-if="isKomisiPusat">
+        <VCol class="d-flex justify-start align-center" cols="2">
+          <VMenu
+            v-model="showFilterMenu"
+            :close-on-content-click="false"
+            offset-y
+          >
+            <template #activator="{ props }">
+              <VBtn
+                color="primary"
+                variant="outlined"
+                v-bind="props"
+                append-icon="ri-filter-fill"
+              >
+                Filter
+              </VBtn>
+            </template>
+            <VCard class="pa-3" width="300">
+              <div class="mt-5">
+                <label>{{ t('task-force.proses-sidang.filter.province') }}</label>
+                <VSelect
+                  v-model="selectedProvince"
+                  :items="filterProvinsi"
+                  class="-mt-10"
+                  item-title="name"
+                  item-value="code"
+                  style="background-color: white;"
+                />
+              </div>
+              <div class="mt-5">
+                <VBtn
+                  style="float: inline-start"
+                  text="Reset Filter"
+                  @click="reset"
+                />
+                <VBtn
+                  style="float: inline-end"
+                  color="primary"
+                  @click="applyFilters"
+                >
+                  Apply Filters
+                </VBtn>
+              </div>
+            </VCard>
+          </VMenu>
+        </VCol>
+      </VRow>
       <VRow v-if="!loadingAll">
         <VCol class="d-flex justify-sm-space-between align-center">
           <VTextField
@@ -131,6 +220,7 @@ const navigateAction = (id: string) => {
                 page: page,
                 size: itemPerPage,
                 keyword: searchQuery,
+                provinsi: selectedProvince.value,
               })
             "
           >
