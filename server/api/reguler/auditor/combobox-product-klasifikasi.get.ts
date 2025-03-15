b4d2e17e7d64197ec6e273b09483331d1de76368
Farezas <fareza087@gmail.com>
2025-03-15 18:03:46 +0700

    feat: combobox product rincian dan klasifikasi

---
+++ server/api/reguler/auditor/combobox-product-klasifikasi.get.ts
@@ -0,0 +1,33 @@
+import type { NuxtError } from "nuxt/app";
+
+export default defineEventHandler(async (event) => {
+  const authorizationHeader = getRequestHeader(event, "Authorization");
+
+  const runtimeConfig = useRuntimeConfig();
+
+  if (typeof authorizationHeader === "undefined") {
+    throw createError({
+      statusCode: 403,
+      statusMessage:
+        "Need to pass valid Bearer-authorization header to access this endpoint",
+    });
+  }
+
+  const { idLayanan } = (await getQuery(event)) as {
+    idLayanan: string;
+  };
+
+  const data = await $fetch<any>(
+    `${runtimeConfig.coreBaseUrl}/api/v1/klasifikasi-product-regular/${idLayanan}/combobox`,
+    {
+      method: "GET",
+      headers: { Authorization: authorizationHeader },
+    }
+  ).catch((err: NuxtError) => {
+    setResponseStatus(event, 400);
+
+    return err.data;
+  });
+
+  return data || null;
+});
