a02d71a8b4711270103eb29e3b4ed0813da13fb2
arvin018 <arvin.aditama@gmail.com>
2025-03-24 09:47:52 +0700

    PROD-494-Pabrik Validasi

--- pages/sh-domestic/submission/self-declare/[id]/edit.vue
+++ pages/sh-domestic/submission/self-declare/[id]/edit.vue
@@ -57,7 +57,7 @@ onMounted(async () => {
             <PelakuUsahaMain />
           </VTabsWindowItem>
           <VTabsWindowItem value="2">
-            <PengajuanMain :id-reg="submissionId" />
+            <PengajuanMain :id-reg="submissionId"  />
           </VTabsWindowItem>
           <VTabsWindowItem value="3">
             <PabrikOutletMain />
