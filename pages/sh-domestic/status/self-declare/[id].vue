4d2d0eaf294aa969aef0249f74e46af21f76835f
geryn25 <35487884+geryn25@users.noreply.github.com>
2025-03-28 22:38:22 +0700

    update if function for cert

--- pages/sh-domestic/status/self-declare/[id].vue
+++ pages/sh-domestic/status/self-declare/[id].vue
@@ -1373,7 +1373,7 @@ const handleCertificate = async (fileName: string, type: string) => {
       },
     });
     if (response) {
-      if ((response.code = 4001)) {
+      if (response.code == 4001) {
         useSnackbar().sendSnackbar(
           "Ada kesalahan saat TTE sertifikat, silahkan coba beberapa saat lag",
           "error"
