990d1c60f8dd329722f5ea3c696594051f04e893
“dev <dev_kemenag@zalarinonatami.com>
2025-04-20 00:00:21 +0700

    add pabrik from lph

---
+++ server/api/reguler/lph/update-fasilitas/add-pabrik-lph.post.ts
@@ -0,0 +1,29 @@
+import type { NuxtError } from 'nuxt/app'
+
+const runtimeConfig = useRuntimeConfig()
+export default defineEventHandler(async (event: any) => {
+  const authorizationHeader = getRequestHeader(event, 'Authorization')
+  if (typeof authorizationHeader === 'undefined') {
+    throw createError({
+      statusCode: 403,
+      statusMessage:
+        'Need to pass valid Bearer-authorization header to access this endpoint',
+    })
+  }
+  const body: any = await readBody(event);
+
+    const data = await $fetch<any>(
+    `${runtimeConfig.coreBaseUrl}/api/v1/lph/halal-certificate-reguler/pabrik`,
+    {
+      method: 'post',
+      headers: { Authorization: authorizationHeader },
+      body: body, 
+    },
+  ).catch((err: NuxtError) => {
+    setResponseStatus(event, 400)
+
+    return err.data
+  })
+
+  return data || null
+})
